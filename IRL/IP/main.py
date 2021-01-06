93af38c02333ae3a9b4ce0c4f8b4e191321c8acc
sw.hong <sw.hong@kaist.ac.kr>
2021-01-06 19:42:43 +0900

    Add: Calculate IOC Loss It doesn't work because q is too small

--- IRL/IP/main.py
+++ IRL/IP/main.py
@@ -1,8 +1,11 @@
 import torch, gym, gym_envs, pickle, random
-from stable_baselines3.common.vec_env import DummyVecEnv
+import numpy as np
+from stable_baselines3.common.vec_env import DummyVecEnv, VecEnv
+from stable_baselines3.common.base_class import BaseAlgorithm
 from algo.torch.ppo import PPO
-from imitation.data import rollout
+from imitation.data import rollout, types
 from torch import nn
+from typing import Sequence
 
 class RewardWrapper(gym.RewardWrapper):
     def __init__(self, env, rwfn):
@@ -24,15 +27,72 @@ class RewfromMat(nn.Module):
     def forward(self, obs):
         return self.layer1(obs)
 
+def generate_trajectories(
+    policy,
+    venv: VecEnv,
+    sample_until: rollout.GenTrajTerminationFn,
+    *,
+    deterministic_policy: bool = False,
+    rng: np.random.RandomState = np.random,
+) -> Sequence[types.TrajectoryWithRew]:
+
+    get_action = policy.forward
+    if isinstance(policy, BaseAlgorithm):
+        policy.set_env(venv)
+
+    trajectories = []
+    trajectories_accum = rollout.TrajectoryAccumulator()
+    obs = venv.reset()
+    for env_idx, ob in enumerate(obs):
+        trajectories_accum.add_step(dict(obs=ob), env_idx)
+
+    active = np.ones(venv.num_envs, dtype=np.bool)
+    while np.any(active):
+        th_obs = torch.as_tensor(obs)
+        acts, _, log_probs = get_action(th_obs)
+        obs, rews, dones, infos = venv.step(acts)
+        infos[0]['log_probs'] = log_probs
+        dones &= active
+
+        new_trajs = trajectories_accum.add_steps_and_auto_finish(
+            acts, obs, rews, dones, infos
+        )
+        trajectories.extend(new_trajs)
+
+        if sample_until(trajectories):
+            active &= ~dones
+    rng.shuffle(trajectories)
+
+    # Sanity checks.
+    for trajectory in trajectories:
+        n_steps = len(trajectory.acts)
+        # extra 1 for the end
+        exp_obs = (n_steps + 1,) + venv.observation_space.shape
+        real_obs = trajectory.obs.shape
+        assert real_obs == exp_obs, f"expected shape {exp_obs}, got {real_obs}"
+        exp_act = (n_steps,) + venv.action_space.shape
+        real_act = trajectory.acts.shape
+        assert real_act == exp_act, f"expected shape {exp_act}, got {real_act}"
+        exp_rew = (n_steps,)
+        real_rew = trajectory.rews.shape
+        assert real_rew == exp_rew, f"expected shape {exp_rew}, got {real_rew}"
+
+    return trajectories
+
 def cal_sample_cost(sampleL, sampleE, rwfn):
-    L_trans = rollout.flatten_trajectories(sampleL)
     E_trans = rollout.flatten_trajectories(sampleE)
-    IOCLoss = 0
+    IOCLoss, w = 0, 0
     for i in range(E_trans.__len__()):
-        IOCLoss += rwfn(torch.from_numpy(E_trans[i]['obs']).float())
-    IOCLoss /= E_trans.__len__()
-    # for i in range(L_trans.__len__()):
-        # IOCLoss +=
+        IOCLoss -= rwfn(torch.from_numpy(E_trans[i]['obs']).float())
+    IOCLoss /= sampleE.__len__()
+    for L_traj in sampleL:
+        c, log_q = 0, 0
+        trans = rollout.flatten_trajectories([L_traj])
+        for i in range(trans.__len__()):
+            c -= rwfn(torch.from_numpy(trans[i]['obs']).float())
+            log_q += trans[i]['infos']['log_probs']
+        w += torch.exp(-c)/torch.exp(log_q)
+    IOCLoss += torch.log(w/sampleE.__len__())
     return IOCLoss
 
 if __name__ == "__main__":
@@ -58,7 +118,7 @@ if __name__ == "__main__":
         # update cost function
         for k in range(10):
             with torch.no_grad():
-                sampleT = rollout.generate_trajectories(algo.policy, env, sample_until) #sample trajectory
+                sampleT = generate_trajectories(algo.policy, env, sample_until) #sample trajectory
             sampleD = random.sample(trajectories, 5) #Expert Demo trajectory
             # todo: cal_sample_cost: calculate IOC Loss using expert and experienced trajectories and rew func.
             IOCLoss = cal_sample_cost(sampleT, sampleD, rwfn) #calculate sample's cost again
