702c48cdd28ac96625a87b6ae74e614ef6ea0683
sw.hong <sw.hong@kaist.ac.kr>
2021-01-06 20:12:43 +0900

    Add: End condition(reached at certain time step) Fix: Randomly select initial point

--- gym_envs/envs/IP_custom_PD.py
+++ gym_envs/envs/IP_custom_PD.py
@@ -2,24 +2,34 @@ import numpy as np
 import math, csv
 from gym import utils, spaces
 from gym.envs.mujoco import mujoco_env
+import os
 
 class IP_custom_PD(mujoco_env.MujocoEnv, utils.EzPickle):
-    def __init__(self):
+    def __init__(self, n_steps):
+        self.traj_len = 0
+        self.n_steps = n_steps
         utils.EzPickle.__init__(self)
-        mujoco_env.MujocoEnv.__init__(self, 'IP_uni.xml', 1)
-        #self.action_space = spaces.Box(low=-np.inf, high=np.inf, shape=(1,), dtype='float64')
-        #self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(2,), dtype='float64')
+        mujoco_env.MujocoEnv.__init__(self, os.path.join(os.path.dirname(__file__), "assets", "IP_custom.xml"), 1)
+        self.action_space = spaces.Box(low=-np.inf, high=np.inf, shape=(1,), dtype='float64')
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(2,), dtype='float64')
+
 
     def step(self, a):
         self.do_simulation(a/100, self.frame_skip)
         ob = self._get_obs()
         rew = - (ob[0] ** 2 + 0.01 * self.sim.data.qfrc_actuator ** 2)
         done = False
-        return ob, rew, done, {}
+        info = {}
+        if self.traj_len == self.n_steps:
+            done = True
+            info = {"terminal observation":ob}
+            self.traj_len = 0
+        self.traj_len += 1
+        return ob, rew, done, info
 
     def reset_model(self):
-        q = np.array([0.5, 3.14])
-       # q = np.concatenate([self.np_random.uniform(size=1, low=-1, high=1), self.np_random.uniform(size=1, low=-1, high=1)])
+        # q = np.array([0.5, 3.14])
+        q = np.concatenate([self.np_random.uniform(size=1, low=-1, high=1), self.np_random.uniform(size=1, low=-1, high=1)])
         self.set_state(np.array([q[1]]), np.array([q[0]]))
         return self._get_obs()
 
