1f57427ed3da38e8a15ce79b911c1b241705cc47
sw.hong <sw.hong@kaist.ac.kr>
2021-01-11 21:48:51 +0900

    Fix: Continuous LQR Expert

--- IRL/IP/CollectExperts.py
+++ IRL/IP/CollectExperts.py
@@ -1,5 +1,7 @@
 import gym, gym_envs
 import torch as th
+import numpy as np
+from scipy import linalg
 from imitation.data import rollout, types
 from stable_baselines3.common.policies import BasePolicy
 from stable_baselines3.common.vec_env import DummyVecEnv
@@ -11,7 +13,14 @@ class ExpertPolicy(BasePolicy):
         super(ExpertPolicy, self).__init__(
             observation_space,
             action_space)
-        self.P, self.D = 5, 1
+        m, g, h, I = 43.16, 9.81, 0.966, 1.4795
+        A = np.array([[0, -m*g*h/I], [1, 0]])
+        B = np.array([[1], [0]])
+        Q = np.array([[1, 0], [0, 1]])
+        R = 0.01
+        X = linalg.solve_continuous_are(A, B, Q, R)
+        K = (1/R * (B.T @ X)).reshape(-1)
+        self.D, self.P = K[0], K[1]
 
     def forward(self):
         return None
