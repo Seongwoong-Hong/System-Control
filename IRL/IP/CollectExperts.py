fb9261c356c425056dd3aec3f1e8f17cd4bf7930
sw.hong <sw.hong@kaist.ac.kr>
2021-01-12 19:59:59 +0900

    Fix: Some mistakes when solving LQR

--- IRL/IP/CollectExperts.py
+++ IRL/IP/CollectExperts.py
@@ -14,10 +14,10 @@ class ExpertPolicy(BasePolicy):
             observation_space,
             action_space)
         m, g, h, I = 43.16, 9.81, 0.966, 1.4795
-        A = np.array([[0, -m*g*h/I], [1, 0]])
-        B = np.array([[1], [0]])
+        A = np.array([[0, m*g*h/I], [1, 0]])
+        B = np.array([[1/I], [0]])
         Q = np.array([[1, 0], [0, 1]])
-        R = 0.01
+        R = 0.001
         X = linalg.solve_continuous_are(A, B, Q, R)
         K = (1/R * (B.T @ X)).reshape(-1)
         self.D, self.P = K[0], K[1]
@@ -26,13 +26,13 @@ class ExpertPolicy(BasePolicy):
         return None
 
     def _predict(self, observation: th.Tensor, deterministic: bool = False) -> th.Tensor:
-        return -(self.P*observation[0][0] + self.D*observation[0][1]).reshape(1, 1)
+        return -(self.P*observation[0][1] + self.D*observation[0][0]).reshape(1, 1)
 
 if __name__ == "__main__":
-    n_steps, n_episodes = 200, 10
+    n_steps, n_episodes = 40, 10
     env = DummyVecEnv([lambda: gym.make("IP_custom-v2", n_steps=n_steps)])
     sample_until = rollout.make_sample_until(n_timesteps=None, n_episodes=n_episodes)
     ExpertPolicy = ExpertPolicy(env.observation_space, env.action_space)
     trajectories = rollout.generate_trajectories(ExpertPolicy, env, sample_until, deterministic_policy=True)
-    types.save("demos/expert.pkl", trajectories)
+    types.save("demos/expert_40.pkl", trajectories)
     print("Expert Trajectories are saved")
