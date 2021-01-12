a62a730c95d720840eb2acb19cb94946ea4b43cd
sw.hong <sw.hong@kaist.ac.kr>
2021-01-12 20:00:43 +0900

    Add: Video recoding callback

--- IRL/IP/main.py
+++ IRL/IP/main.py
@@ -1,10 +1,11 @@
-import torch, gym, pickle
+import torch, gym, gym_envs, pickle, os, sys
 from stable_baselines3.common.vec_env import DummyVecEnv
 from common.callbacks import VideoRecorderCallback
 from common.rollouts import get_trajectories_probs
 from algo.torch.ppo import PPO
 from algo.torch.IRL import RewfromMat
 from imitation.data import rollout
+from mujoco_py import GlfwContext
 
 class RewardWrapper(gym.RewardWrapper):
     def __init__(self, env, rwfn):
@@ -19,17 +20,22 @@ class RewardWrapper(gym.RewardWrapper):
         return self.rwfn.forward(torch.from_numpy(observation).to(rwfn.device))
 
 if __name__ == "__main__":
-    n_steps, n_episodes = 200, 10
-    device = 'cuda'
+    if len(sys.argv) == 1:
+        device = 'cuda:0'
+    else:
+        device = sys.argv[1]
+    n_steps, n_episodes = 40, 10
     env_id = "IP_custom-v2"
-    tensorboard_dir = "tmp/log"
-    model_dir = "tmp/model"
+    log_dir = os.path.join(os.path.dirname(__file__), "tmp", "log")
+    model_dir = os.path.join(os.path.dirname(__file__), "tmp", "model")
+    expert_dir = os.path.join(os.path.dirname(__file__), "demos", "expert_20.pkl")
     env = gym.make(env_id, n_steps=n_steps)
     num_obs = env.observation_space.shape[0]
     rwfn = RewfromMat(num_obs, device=device).double().to(device)
     env = DummyVecEnv([lambda: env])
     sample_until = rollout.make_sample_until(n_timesteps=None, n_episodes=n_episodes)
     print("Start Guided Cost Learning...  Using {} environment".format(env_id))
+    GlfwContext(offscreen=True)
     algo = PPO("MlpPolicy",
                env=env,
                n_steps=2048,
@@ -39,9 +45,12 @@ if __name__ == "__main__":
                ent_coef=0.01,
                verbose=1,
                device=device,
-               tensorboard_log=tensorboard_dir)
-    video_recorder = VideoRecorderCallback(gym.make(env_id, n_steps=n_steps), render_freq=50000)
-    with open("demos/expert.pkl", "rb") as f:
+               tensorboard_log=log_dir)
+    video_recorder = VideoRecorderCallback(log_dir+"/video",
+                                           gym.make(env_id, n_steps=n_steps),
+                                           n_eval_episodes=5,
+                                           render_freq=512000)
+    with open(expert_dir, "rb") as f:
         expert_trajs = pickle.load(f)
         learner_trajs = []
     for _ in range(10):
@@ -55,8 +64,8 @@ if __name__ == "__main__":
             rwfn.learn(epoch=50)
 
         # update policy
-        env = DummyVecEnv([lambda: RewardWrapper(gym.make(env_id, n_steps=n_steps), rwfn.eval())])
+        env = DummyVecEnv([lambda: RewardWrapper(gym.make(env_id, n_steps=n_steps), rwfn._eval())])
         algo.set_env(env)
         algo.learn(total_timesteps=1024000, callback=video_recorder)
-    torch.save(rwfn, model_dir+"rwfn.pt")
-    algo.save(model_dir+"ppo")
+    torch.save(rwfn, model_dir+"/rwfn.pt")
+    algo.save(model_dir+"/ppo")
