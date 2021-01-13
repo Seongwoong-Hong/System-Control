3e762be7c26cb9befb11f6b0226e0fe065efda16
sw.hong <sw.hong@kaist.ac.kr>
2021-01-13 21:19:41 +0900

    Add: Separate Wrappers

---
+++ common/wrappers.py
@@ -0,0 +1,36 @@
+import gym, torch
+import numpy as np
+
+class ActionRewardWrapper(gym.RewardWrapper):
+    def __init__(self, env, rwfn):
+        super(ActionRewardWrapper, self).__init__(env)
+        self.rwfn = rwfn
+
+    def action(self, action):
+        return action
+
+    def step(self, action):
+        observation, reward, done, info = self.env.step(self.action(action))
+        return observation, self.reward(observation, self.action(action)), done, info
+
+    def reward(self, observation, action):
+        rwinp = torch.from_numpy(np.append(observation, action)).to(self.rwfn.device)
+        return self.rwfn.forward(rwinp)
+
+class ActionWrapper(gym.ActionWrapper):
+    def action(self, action):
+        return action
+
+
+class RewardWrapper(gym.RewardWrapper):
+    def __init__(self, env, rwfn):
+        super(RewardWrapper, self).__init__(env)
+        self.rwfn = rwfn
+
+    def step(self, action):
+        observation, reward, done, info = self.env.step(action)
+        return observation, self.reward(observation, action), done, info
+
+    def reward(self, observation, action):
+        rwinp = torch.from_numpy(np.append(observation, action)).to(self.rwfn.device)
+        return self.rwfn.forward(rwinp)
