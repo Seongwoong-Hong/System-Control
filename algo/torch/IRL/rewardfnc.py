4b5e033966828976ea8d0396ba56044ea8e9b7aa
sw.hong <sw.hong@kaist.ac.kr>
2021-01-13 21:24:10 +0900

    Fix: Now, don't have to calculate all timesteps per each transition.

--- algo/torch/IRL/rewardfnc.py
+++ algo/torch/IRL/rewardfnc.py
@@ -1,4 +1,4 @@
-import torch, random
+import torch, random, time
 from torch import nn
 
 class RewfromMat(nn.Module):
@@ -6,7 +6,7 @@ class RewfromMat(nn.Module):
                  inp,
                  device='cpu',
                  optimizer_class=torch.optim.Adam,
-                 lr=1e-4):
+                 lr=3e-5):
         super(RewfromMat, self).__init__()
         self.device = device
         self.layer1 = nn.Linear(inp, inp)
@@ -34,23 +34,30 @@ class RewfromMat(nn.Module):
         self._train()
         for _ in range(epoch):
             IOCLoss = 0.0
+            # Calculate learned cost loss
             for E_trans in self.sampleE:
                 for i in range(len(E_trans)):
                     IOCLoss -= self.forward(E_trans[i]['infos']['rwinp'])
             IOCLoss /= len(self.sampleE)
-            for trans_j in self.sampleE+self.sampleL:
-                cost, wjZ = 0, 0
+            # Calculate Max Ent. Loss
+            wjr, wjp = [], []
+            with torch.no_grad():
+                for trans_k in self.sampleE + self.sampleL:
+                    r, p = 0, 0
+                    for t in range(len(trans_k)):
+                        r += self.forward(trans_k[t]['infos']['rwinp'])
+                        p += trans_k[t]['infos']['log_probs']
+                    wjr.append(r)
+                    wjp.append(-p)
+            for j in range(len(self.sampleE+self.sampleL)):
+                trans_j = (self.sampleE+self.sampleL)[j]
+                cost, Zwjs = 0, 0
+                for k in range(len(self.sampleE+self.sampleL)):
+                    s = wjr[k] - wjr[j] + wjp[k] - wjp[j]
+                    Zwjs += torch.exp(wjr[k] - wjr[j] + wjp[k] - wjp[j])
                 for t in range(len(trans_j)):
                     cost -= self.forward(trans_j[t]['infos']['rwinp'])
-                for trans_k in self.sampleE+self.sampleL:
-                    temp = 0
-                    with torch.no_grad():
-                        for t in range(len(trans_k)):
-                            temp += -self.forward(trans_k[t]['infos']['rwinp']) \
-                                    + self.forward(trans_j[t]['infos']['rwinp']) \
-                                    - trans_k[t]['infos']['log_probs'] + trans_j[t]['infos']['log_probs']
-                        wjZ += torch.exp(temp)
-                IOCLoss -= cost / wjZ
+                IOCLoss -= cost / Zwjs
             self.optimizer.zero_grad()
             IOCLoss.backward()
             self.optimizer.step()
