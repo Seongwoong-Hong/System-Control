ab1b35a62a5b48f9528f103bc16624835a545d54
sw.hong <sw.hong@kaist.ac.kr>
2021-01-14 13:04:07 +0900

    Fix: Made more general using input arguments

--- IRL/IP/main.py
+++ IRL/IP/main.py
@@ -1,40 +1,36 @@
 import torch, gym, gym_envs, pickle, os, sys
 from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.monitor import Monitor
 from common.callbacks import VideoRecorderCallback
 from common.rollouts import get_trajectories_probs
+from common.wrappers import RewardWrapper
 from algo.torch.ppo import PPO
 from algo.torch.IRL import RewfromMat
 from imitation.data import rollout
 from mujoco_py import GlfwContext
 
-class RewardWrapper(gym.RewardWrapper):
-    def __init__(self, env, rwfn):
-        super(RewardWrapper, self).__init__(env)
-        self.rwfn = rwfn
-
-    def step(self, action):
-        observation, reward, done, info = self.env.step(action)
-        return observation, self.reward(observation), done, info
-
-    def reward(self, observation):
-        return self.rwfn.forward(torch.from_numpy(observation).to(rwfn.device))
-
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        device = 'cuda:0'
+        raise SyntaxError("Please enter name for logging")
+    elif len(sys.argv) == 2:
+        name = sys.argv[1]
+    elif len(sys.argv) == 3:
+        name = sys.argv[1]
+        device = sys.argv[2]
     else:
-        device = sys.argv[1]
+        raise SyntaxError("Too many system inputs")
     n_steps, n_episodes = 40, 10
     env_id = "IP_custom-v2"
     log_dir = os.path.join(os.path.dirname(__file__), "tmp", "log")
     model_dir = os.path.join(os.path.dirname(__file__), "tmp", "model")
-    expert_dir = os.path.join(os.path.dirname(__file__), "demos", "expert_20.pkl")
+    expert_dir = os.path.join(os.path.dirname(__file__), "demos", "expert_bar_40.pkl")
     env = gym.make(env_id, n_steps=n_steps)
     num_obs = env.observation_space.shape[0]
-    rwfn = RewfromMat(num_obs, device=device).double().to(device)
+    num_act = env.action_space.shape[0]
+    rwfn = RewfromMat(num_obs+num_act, device=device).double().to(device)
     env = DummyVecEnv([lambda: env])
     sample_until = rollout.make_sample_until(n_timesteps=None, n_episodes=n_episodes)
-    print("Start Guided Cost Learning...  Using {} environment".format(env_id))
+    print("Start Guided Cost Learning...  Using {} environment.\nName for logging is {}".format(env_id, name))
     GlfwContext(offscreen=True)
     algo = PPO("MlpPolicy",
                env=env,
@@ -43,13 +39,13 @@ if __name__ == "__main__":
                gamma=0.99,
                gae_lambda=0.95,
                ent_coef=0.01,
-               verbose=1,
+               verbose=0,
                device=device,
                tensorboard_log=log_dir)
-    video_recorder = VideoRecorderCallback(log_dir+"/video",
+    video_recorder = VideoRecorderCallback(log_dir+"/video/bar_2",
                                            gym.make(env_id, n_steps=n_steps),
                                            n_eval_episodes=5,
-                                           render_freq=512000)
+                                           render_freq=102400)
     with open(expert_dir, "rb") as f:
         expert_trajs = pickle.load(f)
         learner_trajs = []
@@ -66,6 +62,6 @@ if __name__ == "__main__":
         # update policy
         env = DummyVecEnv([lambda: RewardWrapper(gym.make(env_id, n_steps=n_steps), rwfn._eval())])
         algo.set_env(env)
-        algo.learn(total_timesteps=1024000, callback=video_recorder)
-    torch.save(rwfn, model_dir+"/rwfn.pt")
-    algo.save(model_dir+"/ppo")
+        algo.learn(total_timesteps=512000, callback=video_recorder, tb_log_name=name)
+    torch.save(rwfn, model_dir+"/"+name+"_rwfn.pt")
+    algo.save(model_dir+"/"+name+"_ppo")
