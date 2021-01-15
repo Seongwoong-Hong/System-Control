24b2885b17bdfe7fa7d192ed3da37dbef036b23a
sw.hong <sw.hong@kaist.ac.kr>
2021-01-15 19:44:37 +0900

    Fix: Test cost function learning

--- IRL/IP/test/test_learning_rwfn.py
+++ IRL/IP/test/test_learning_rwfn.py
@@ -3,19 +3,20 @@ import torch, gym, gym_envs, pickle, os
 from stable_baselines3.common.vec_env import DummyVecEnv
 from common.rollouts import get_trajectories_probs
 from algo.torch.ppo import PPO
-from algo.torch.IRL import RewfromMat
+from common.modules import NNCost
 from imitation.data import rollout
-from common.wrappers import ActionRewardWrapper
+from common.wrappers import CostWrapper
 
 if __name__ == "__main__":
     n_steps, n_episodes = 40, 10
     env_id = "IP_custom-v2"
-    expert_dir = os.path.join("..", "demos", "expert_bar_aw_40.pkl")
+    expert_dir = os.path.join("..", "demos", "expert_bar_100.pkl")
     env = gym.make(env_id, n_steps=n_steps)
     num_obs = env.observation_space.shape[0]
     num_act = env.action_space.shape[0]
-    rwfn = RewfromMat(num_obs+num_act, device='cpu').double().to('cpu')
-    env = DummyVecEnv([lambda: ActionRewardWrapper(env, rwfn)])
+    inp = num_obs+num_act
+    costfn = NNCost(arch=[inp, 4*inp, 2*inp], device='cpu').double().to('cpu')
+    env = DummyVecEnv([lambda: CostWrapper(env, costfn)])
     sample_until = rollout.make_sample_until(n_timesteps=None, n_episodes=n_episodes)
     print("Start Guided Cost Learning...  Using {} environment".format(env_id))
     algo = PPO("MlpPolicy",
@@ -31,11 +32,15 @@ if __name__ == "__main__":
     with open(expert_dir, "rb") as f:
         expert_trajs = pickle.load(f)
         learner_trajs = []
+    for _ in range(20):
+        for k in range(1):
+            with torch.no_grad():
+                learner_trajs += rollout.generate_trajectories(algo.policy, env, sample_until)
+                expert_trans = get_trajectories_probs(expert_trajs, algo.policy)
+                learner_trans = get_trajectories_probs(learner_trajs, algo.policy)
+            costfn.sample_trajectory_sets(learner_trans, expert_trans)
+            costfn.learn(epoch=10)
 
-    for k in range(20):
-        with torch.no_grad():
-            learner_trajs += rollout.generate_trajectories(algo.policy, env, sample_until)
-            expert_trans = get_trajectories_probs(expert_trajs, algo.policy)
-            learner_trans = get_trajectories_probs(learner_trajs, algo.policy)
-        rwfn.sample_trajectory_sets(learner_trans, expert_trans)
-        rwfn.learn(epoch=50)
+        env = DummyVecEnv([lambda: CostWrapper(gym.make(env_id, n_steps=n_steps), costfn._eval())])
+        algo.set_env(env)
+        algo.learn(total_timesteps=512000)
