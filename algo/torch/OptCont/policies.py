b8b78ae9508c6738a4f7a7b25a2b63243a3d0d05
sw.hong <sw.hong@kaist.ac.kr>
2021-01-15 13:24:51 +0900

    Add: Made general LQR policy that can implemented every environment if it has defined.

---
+++ algo/torch/OptCont/policies.py
@@ -0,0 +1,31 @@
+import gym
+import torch as th
+import numpy as np
+from scipy import linalg
+from stable_baselines3.common.policies import BasePolicy
+
+class LQRPolicy(BasePolicy):
+    def __init__(self,
+                 observation_space:gym.spaces.Space,
+                 action_space:gym.spaces.Space):
+
+        super(LQRPolicy, self).__init__(
+            observation_space,
+            action_space)
+
+        self.P, self.D = self._get_gains()
+
+    def _get_gains(self):
+        A, B, Q, R = self._build_env()
+        X = linalg.solve_continuous_are(A, B, Q, R)
+        K = (1/R * (B.T @ X)).reshape(-1)
+        return K[1], K[0]
+
+    def _build_env(self):
+        return NotImplementedError
+
+    def forward(self):
+        return None
+
+    def _predict(self, observation: th.Tensor, deterministic: bool = False) -> th.Tensor:
+        return -(self.P*observation[0][1] + self.D*observation[0][0]).reshape(1, 1)
