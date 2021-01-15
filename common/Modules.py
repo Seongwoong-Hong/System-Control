e4af9227fc24ab496c2b637528441b4759e8d7ec
sw.hong <sw.hong@kaist.ac.kr>
2021-01-15 15:58:41 +0900

    Fix: Change reward function to cost function Fix: Change directory

---
+++ common/Modules.py
@@ -0,0 +1,80 @@
+import torch, random, time
+from torch import nn
+
+class NNCost(nn.Module):
+    def __init__(self,
+                 inp,
+                 device='cpu',
+                 optimizer_class=torch.optim.Adam,
+                 lr=3e-5):
+        super(NNCost, self).__init__()
+        self.device = device
+        self.layer1 = nn.Linear(inp, 4*inp)
+        self.layer2 = nn.Linear(4*inp, 2*inp)
+        self.layer3 = nn.Linear(2*inp, 1)
+        self.tanh = nn.Tanh()
+        self.optimizer_class = optimizer_class
+        self._build(lr)
+        self.evalmod = False
+
+    def _build(self, lr):
+        self.optimizer = self.optimizer_class(self.parameters(), lr)
+
+    def sample_trajectory_sets(self, learner_trans, expert_trans):
+        self.sampleL = random.sample(learner_trans, 5)
+        self.sampleE = random.sample(expert_trans, 10)
+
+    def forward(self, obs):
+        if self.evalmod:
+            with torch.no_grad():
+                out = self.tanh(self.layer1(obs))
+                out = self.tanh(self.layer2(out))
+                out = self.tanh(self.layer3(out))
+                return out
+        else:
+            out = self.tanh(self.layer1(obs))
+            out = self.tanh(self.layer2(out))
+            out = self.tanh(self.layer3(out))
+            return out
+
+    def learn(self, epoch):
+        self._train()
+        for _ in range(epoch):
+            IOCLoss1, IOCLoss2 = 0, 0
+            # Calculate learned cost loss
+            for E_trans in self.sampleE:
+                for i in range(len(E_trans)):
+                    IOCLoss1 += self.forward(E_trans[i]['infos']['cost_inp'])
+            IOCLoss1 /= len(self.sampleE)
+            # Calculate Max Ent. Loss
+            wjr, wjp = [], []
+            with torch.no_grad():
+                for trans_k in self.sampleE + self.sampleL:
+                    r, p = 0, 0
+                    for t in range(len(trans_k)):
+                        r -= self.forward(trans_k[t]['infos']['cost_inp'])
+                        p += trans_k[t]['infos']['log_probs']
+                    wjr.append(r)
+                    wjp.append(p)
+            for j in range(len(self.sampleE+self.sampleL)):
+                trans_j = (self.sampleE+self.sampleL)[j]
+                cost, Zwjs = 0, 0
+                for k in range(len(self.sampleE+self.sampleL)):
+                    Zwjs += torch.exp(wjr[k] - wjr[j]) / torch.exp(wjp[k] - wjp[j])
+                for t in range(len(trans_j)):
+                    cost += self.forward(trans_j[t]['infos']['cost_inp'])
+                IOCLoss2 -= cost / Zwjs
+            IOCLoss = IOCLoss1 + IOCLoss2
+            self.optimizer.zero_grad()
+            IOCLoss.backward()
+            self.optimizer.step()
+        print("Loss for Expert cost: {:.2f}, Loss for Max Ent.: {:.2f}".format(IOCLoss1.item(), IOCLoss2.item()))
+        return self
+
+    def _train(self):
+        self.evalmod = False
+        return self.train()
+
+    def _eval(self):
+        self.evalmod = True
+        return self.eval()
