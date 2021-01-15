a663d4ed82c6f47e1869dd6b2f3c0f496ac188b3
sw.hong <sw.hong@kaist.ac.kr>
2021-01-15 17:09:31 +0900

    Add: CostWrapper - changed the cost to reward for the policy gradient learning

--- common/wrappers.py
+++ common/wrappers.py
@@ -21,7 +21,6 @@ class ActionWrapper(gym.ActionWrapper):
     def action(self, action):
         return action
 
-
 class RewardWrapper(gym.RewardWrapper):
     def __init__(self, env, rwfn):
         super(RewardWrapper, self).__init__(env)
@@ -34,3 +33,17 @@ class RewardWrapper(gym.RewardWrapper):
     def reward(self, observation, action):
         rwinp = torch.from_numpy(np.append(observation, action)).to(self.rwfn.device)
         return self.rwfn.forward(rwinp)
+
+
+class CostWrapper(gym.RewardWrapper):
+    def __init__(self, env, rwfn):
+        super(CostWrapper, self).__init__(env)
+        self.rwfn = rwfn
+
+    def step(self, action):
+        observation, reward, done, info = self.env.step(action)
+        return observation, self.reward(observation, action), done, info
+
+    def reward(self, observation, action):
+        rwinp = torch.from_numpy(np.append(observation, action)).to(self.rwfn.device)
+        return -self.rwfn.forward(rwinp)
