4d90ae4be1c0361ed919a903de7856173e2a5bfb
sw.hong <sw.hong@kaist.ac.kr>
2021-01-17 20:32:14 +0900

    Fix: Save running files

--- IRL/IP/main.py
+++ IRL/IP/main.py
@@ -1,6 +1,5 @@
-import torch, gym, gym_envs, pickle, os, sys
+import torch, gym, gym_envs, pickle, os, sys, shutil
 from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3.common.monitor import Monitor
 from common.callbacks import VideoRecorderCallback
 from common.rollouts import get_trajectories_probs
 from common.wrappers import CostWrapper
@@ -20,16 +19,24 @@ if __name__ == "__main__":
         device = sys.argv[2]
     else:
         raise SyntaxError("Too many system inputs")
-    n_steps, n_episodes = 100, 10
+    n_steps, n_episodes = 100, 5
     env_id = "IP_custom-v2"
-    log_dir = os.path.join(os.path.dirname(__file__), "tmp", "log")
+    log_dir = os.path.join(os.path.dirname(__file__), "tmp", "log", name)
+    if not os.path.isdir(log_dir):
+        os.mkdir(log_dir)
     model_dir = os.path.join(os.path.dirname(__file__), "tmp", "model")
     expert_dir = os.path.join(os.path.dirname(__file__), "demos", "expert_bar_100.pkl")
+
+    ## Copy used file to logging folder
+    shutil.copy(os.path.abspath("../../common/modules.py"), log_dir)
+    shutil.copy(os.path.abspath("../../gym_envs/envs/IP_custom_PD.py"), log_dir)
+    shutil.copy(os.path.abspath(__file__), log_dir)
+
     env = gym.make(env_id, n_steps=n_steps)
     num_obs = env.observation_space.shape[0]
     num_act = env.action_space.shape[0]
     inp = num_obs+num_act
-    costfn = NNCost(arch=[inp, 4*inp, 2*inp], device=device).double().to(device)
+    costfn = NNCost(arch=[inp], device=device, num_samp=5).double().to(device)
     env = DummyVecEnv([lambda: env])
     sample_until = rollout.make_sample_until(n_timesteps=None, n_episodes=n_episodes)
     print("Start Guided Cost Learning...  Using {} environment.\nThe Name for logging is {}".format(env_id, name))
@@ -44,16 +51,16 @@ if __name__ == "__main__":
                verbose=0,
                device=device,
                tensorboard_log=log_dir)
-    video_recorder = VideoRecorderCallback(log_dir+"/video/bar_2",
+    video_recorder = VideoRecorderCallback(log_dir+"/video/"+name,
                                            gym.make(env_id, n_steps=n_steps),
                                            n_eval_episodes=5,
                                            render_freq=102400)
     with open(expert_dir, "rb") as f:
         expert_trajs = pickle.load(f)
         learner_trajs = []
-    for _ in range(10):
+    for _ in range(40):
         # update cost function
-        for k in range(20):
+        for k in range(10):
             with torch.no_grad():
                 learner_trajs += rollout.generate_trajectories(algo.policy, env, sample_until)
                 expert_trans = get_trajectories_probs(expert_trajs, algo.policy)
@@ -61,9 +68,9 @@ if __name__ == "__main__":
             costfn.sample_trajectory_sets(learner_trans, expert_trans)
             costfn.learn(epoch=50)
 
-        # update policy
+        # update policy using PPO
         env = DummyVecEnv([lambda: CostWrapper(gym.make(env_id, n_steps=n_steps), costfn._eval())])
         algo.set_env(env)
-        algo.learn(total_timesteps=512000, callback=video_recorder, tb_log_name=name)
+        algo.learn(total_timesteps=409600, callback=video_recorder, tb_log_name=name)
     torch.save(costfn, model_dir+"/"+name+"_costfn.pt")
     algo.save(model_dir+"/"+name+"_ppo")
