f4416c26a2e61163c17f795fa8d75c893cf02df9
sw.hong <sw.hong@kaist.ac.kr>
2021-01-20 19:04:58 +0900

    Fix: Modification according to another files

--- IRL/IP/test/test_expert_traj.py
+++ IRL/IP/test/test_expert_traj.py
@@ -1,44 +1,43 @@
 import gym, gym_envs, torch, os, pickle, time
 import numpy as np
-from stable_baselines3.common.vec_env import DummyVecEnv
-from algo.torch.IRL import RewfromMat
-from algo.torch.ppo import PPO
-from common.rollouts import get_trajectories_probs
+from algo.torch.OptCont import LQRPolicy
+from imitation.data.rollout import flatten_trajectories
 from matplotlib import pyplot as plt
 
-class RewardWrapper(gym.RewardWrapper):
-    def __init__(self, env, rwfn):
-        super(RewardWrapper, self).__init__(env)
-        self.rwfn = rwfn
-
-    def step(self, action):
-        observation, reward, done, info = self.env.step(action)
-        return observation, self.reward(observation), done, info
-
-    def reward(self, observation):
-        return self.rwfn.forward(torch.from_numpy(observation).to(self.rwfn.device))
+class ExpertPolicy(LQRPolicy):
+    def _build_env(self):
+        m, g, h, I = 5.0, 9.81, 0.5, 1.667
+        self.Q = np.array([[1, 0], [0, 1]])
+        self.R = 0.001 * self.gear ** 2
+        self.A = np.array([[0, m*g*h/I], [1, 0]])
+        self.B = np.array([[1/I], [0]])
+        return self.A, self.B, self.Q, self.R
 
 model_dir = os.path.join("..", "tmp", "model")
-rwfn = torch.load(model_dir + "/f_rwfn.pt")
-algo = PPO.load(model_dir + "/f_ppo.zip")
-env = DummyVecEnv([lambda: gym.make("IP_custom-v2", n_steps=400)])
-expert_dir = os.path.join("..", "demos", "expert_bar_40.pkl")
+env = gym.make("IP_custom-v2")
+exp = ExpertPolicy(env)
+expert_dir = os.path.join("..", "demos", "expert_bar_100.pkl")
 with open(expert_dir, "rb") as f:
     expert_trajs = pickle.load(f)
     learner_trajs = []
 
-obs_list = []
+obs_list, cost_list = [], []
+Q, R, dt = exp.Q, exp.R, env.dt
 env.reset()
-expert_trans = get_trajectories_probs(expert_trajs, algo.policy)
-for tran in expert_trans:
-    env.env_method('set_state', np.array([tran[0]['obs'][1]]), np.array([tran[0]['obs'][0]]))
-    obs = env.env_method('_get_obs')
-    obs_list.append(obs[0].reshape(-1))
+
+for traj in expert_trajs:
+    tran = flatten_trajectories([traj])
+    env.set_state(np.array([tran[0]['obs'][1]]), np.array([tran[0]['obs'][0]]))
+    obs = env._get_obs()
+    obs_list.append(obs.reshape(-1))
     env.render()
     for t in range(len(tran)):
-        obs, _, _, _ = env.step(tran[t]['acts'])
+        act = tran[t]['acts']
+        obs, _, _, _ = env.step(act)
+        cost = obs @ Q @ obs.T + act*act*R
+        cost_list.append(cost.reshape(-1))
         obs_list.append(obs.reshape(-1))
         env.render()
-        time.sleep(0.05)
+        time.sleep(dt)
 
 print('end')
