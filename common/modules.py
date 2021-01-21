b9babef9172db34bd61c12e671d2133b0987a77c
sw.hong <sw.hong@kaist.ac.kr>
2021-01-21 17:16:57 +0900

    Fix: Minor modification

--- common/modules.py
+++ common/modules.py
@@ -1,19 +1,25 @@
 import torch, random, time
 from torch import nn
+from typing import List
 
 class NNCost(nn.Module):
     def __init__(self,
-                 arch,
+                 arch: List[int],
                  device='cpu',
                  optimizer_class=torch.optim.Adam,
                  act_fcn=nn.Tanh,
-                 lr=3e-5):
+                 lr: float = 3e-5,
+                 num_expert: int = 10,
+                 num_samp: int = 10):
+        # The first argument of the arch must be an input size
         super(NNCost, self).__init__()
         self.device = device
         self.optimizer_class = optimizer_class
         self.act_fnc = act_fcn
         self._build(lr, arch)
         self.evalmod = False
+        self.num_expert = num_expert
+        self.num_samp = num_samp
 
     def _build(self, lr, arch):
         layers = []
@@ -22,20 +28,21 @@ class NNCost(nn.Module):
             layers.append(self.act_fnc())
         layers.append(nn.Linear(arch[-1], 1))
         self.layers = nn.Sequential(*layers)
+        self.aparam = nn.Linear(1, 1, bias=False)
         self.optimizer = self.optimizer_class(self.parameters(), lr)
 
     def sample_trajectory_sets(self, learner_trans, expert_trans):
-        self.sampleE = random.sample(expert_trans, 10)
-        self.sampleL = random.sample(learner_trans, 10)
+        self.sampleE = random.sample(expert_trans, self.num_expert)
+        self.sampleL = random.sample(learner_trans, self.num_samp)
 
     def forward(self, obs):
         if self.evalmod:
             with torch.no_grad():
-                return self.layers(obs)
+                return self.layers(obs[:-1])**2 + self.aparam(obs[-1:])**2
         else:
-            return self.layers(obs)
+            return self.layers(obs[:-1])**2 + self.aparam(obs[-1:])**2
 
-    def learn(self, epoch):
+    def learn(self, epoch: int):
         self._train()
         for _ in range(epoch):
             IOCLoss1, IOCLoss2 = 0, 0
@@ -58,7 +65,7 @@ class NNCost(nn.Module):
                 trans_j = (self.sampleE+self.sampleL)[j]
                 cost, Zwjs = 0, 0
                 for k in range(len(self.sampleE+self.sampleL)):
-                    Zwjs += torch.exp(wjr[k] - wjr[j]) / torch.exp(wjp[k] - wjp[j])
+                    Zwjs += torch.exp(wjr[k] - wjr[j] - wjp[k] + wjp[j])
                 for t in range(len(trans_j)):
                     cost += self.forward(trans_j[t]['infos']['rwinp'])
                 IOCLoss2 -= cost / Zwjs
