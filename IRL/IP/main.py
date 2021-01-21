d1f8b810ca90972869b0e519a31cc046d9a39d19
sw.hong <sw.hong@kaist.ac.kr>
2021-01-21 17:26:06 +0900

    Fix: Modify wrong trajectory sampling

--- IRL/IP/main.py
+++ IRL/IP/main.py
@@ -5,6 +5,7 @@ from common.rollouts import get_trajectories_probs
 from common.wrappers import CostWrapper
 from common.modules import NNCost
 from algo.torch.ppo import PPO
+from algo.torch.sac import SAC
 from imitation.data import rollout
 from mujoco_py import GlfwContext
 
@@ -41,7 +42,7 @@ if __name__ == "__main__":
     num_obs = env.observation_space.shape[0]
     num_act = env.action_space.shape[0]
     inp = num_obs+num_act
-    costfn = NNCost(arch=[num_obs], device=device, num_samp=n_episodes, lr=1e-4).double().to(device)
+    costfn = NNCost(arch=[num_obs], device=device, num_expert=5, num_samp=n_episodes, lr=1e-4).double().to(device)
     env = DummyVecEnv([lambda: CostWrapper(env, costfn)])
     sample_until = rollout.make_sample_until(n_timesteps=None, n_episodes=n_episodes)
     print("Start Guided Cost Learning...  Using {} environment.\nThe Name for logging is {}".format(env_id, name))
@@ -53,10 +54,11 @@ if __name__ == "__main__":
                batch_size=128,
                gamma=0.99,
                gae_lambda=0.95,
-               ent_coef=0.01,
+               ent_coef=0.1,
                verbose=0,
                device=device,
                tensorboard_log=log_dir)
+
     video_recorder = VideoRecorderCallback(log_dir+"/video/"+name,
                                            gym.make(env_id, n_steps=n_steps),
                                            n_eval_episodes=5,
@@ -65,19 +67,21 @@ if __name__ == "__main__":
         expert_trajs = pickle.load(f)
         learner_trajs = []
     for i in range(50):
+        if i > 4:
+            shutil.rmtree(os.path.join(log_dir, name) + "_%d" % (i - 4))
+        with torch.no_grad():
+            learner_trajs += rollout.generate_trajectories(algo.policy, env, sample_until)
+            expert_trans = get_trajectories_probs(expert_trajs, algo.policy)
+            learner_trans = get_trajectories_probs(learner_trajs, algo.policy)
         # update cost function
-        for k in range(10):
-            with torch.no_grad():
-                learner_trajs += rollout.generate_trajectories(algo.policy, env, sample_until)
-                expert_trans = get_trajectories_probs(expert_trajs, algo.policy)
-                learner_trans = get_trajectories_probs(learner_trajs, algo.policy)
+        for k in range(50):
             costfn.sample_trajectory_sets(learner_trans, expert_trans)
-            costfn.learn(epoch=50)
-
+            costfn.learn(epoch=10)
+        print("Now start policy optimization...")
         # update policy using PPO
         env = DummyVecEnv([lambda: CostWrapper(gym.make(env_id, n_steps=n_steps), costfn._eval())])
         algo.set_env(env)
         algo.learn(total_timesteps=204800, callback=video_recorder, tb_log_name=name)
         if (i+1) % 5 == 0:
             torch.save(costfn, model_dir+"/costfn{}.pt".format(i+1))
-            algo.save(model_dir+"/ppo{}".format(i+1))
+            algo.save(model_dir+"/sac{}".format(i+1))
