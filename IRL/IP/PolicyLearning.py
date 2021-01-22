882a94362b69ee85337240e1e70d77fe8733ed1e
sw.hong <sw.hong@kaist.ac.kr>
2021-01-22 14:58:57 +0900

    Fix: Made easier to test variety policies

--- IRL/IP/PolicyLearning.py
+++ IRL/IP/PolicyLearning.py
@@ -5,12 +5,13 @@ from algo.torch.ppo import PPO
 from common.wrappers import CostWrapper
 
 if __name__ == "__main__":
-    model_dir = os.path.join("..", "tmp", "model")
-    costfn = torch.load(model_dir + "/bar10_costfn.pt")
-    env_id = "IP_custom-v2"
+    name = "2021-1-22-11-3-31"
+    num = 10
+    model_dir = os.path.join("tmp", "log", name, "model")
+    costfn = torch.load(model_dir + "/costfn{}.pt".format(num))
+    env_id = "IP_custom-v1"
     n_steps = 100
-    device = "cuda:1"
-    name = "bar10_extra"
+    device = "cuda:3"
     current_path = os.path.dirname(__file__)
     log_dir = os.path.join(current_path, "tmp", "log", name)
     if not os.path.isdir(log_dir):
@@ -24,7 +25,8 @@ if __name__ == "__main__":
                gamma=0.99,
                gae_lambda=0.95,
                ent_coef=0.01,
-               verbose=0,
+               verbose=1,
                device=device,
                tensorboard_log=log_dir)
-    algo.learn(total_timesteps=1024000, tb_log_name=name)
+    algo.learn(total_timesteps=409600, tb_log_name=name+"_extra")
+    algo.save(model_dir+"/extra_ppo")
