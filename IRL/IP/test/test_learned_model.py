7b51555b40cc2995fdda5101535215dc44a4d306
sw.hong <sw.hong@kaist.ac.kr>
2021-01-22 16:34:06 +0900

    Fix: Made easier to test variety policies

--- IRL/IP/test/test_learned_model.py
+++ IRL/IP/test/test_learned_model.py
@@ -1,9 +1,8 @@
-import os, torch, gym, gym_envs, time, cv2
+import os, torch, gym, gym_envs, time
 import numpy as np
 from algo.torch.ppo import PPO
 from algo.torch.OptCont.policies import LQRPolicy
-from matplotlib import pyplot as plt
-from mujoco_py import GlfwContext
+from common.wrappers import CostWrapper
 
 class ExpertPolicy(LQRPolicy):
     def _build_env(self):
@@ -14,59 +13,25 @@ class ExpertPolicy(LQRPolicy):
         self.B = np.array([[1/I], [0]])
         return self.A, self.B, self.Q, self.R
 
-GlfwContext(offscreen=True)
-model_dir = os.path.join("..", "tmp", "model")
-# rwfn = torch.load(model_dir + "/bar1_rwfn.pt")
-algo = PPO.load(model_dir + "/bar7_ppo.zip")
-env = gym.make("IP_custom-v2", n_steps=100)
+name = "2021-1-22-11-3-31"
+num = 10
+model_dir = os.path.join("..", "tmp", "log", name, "model")
+costfn = torch.load(model_dir + "/costfn{}.pt".format(num))
+algo = PPO.load(model_dir + "/extra_ppo.zip")
+# algo = PPO.load(model_dir + "/ppo{}.zip".format(num))
+env = CostWrapper(gym.make("IP_custom-v1", n_steps=100), costfn)
 exp = ExpertPolicy(env)
 dt = env.dt
-q = np.array([0.12, 0.06])
+q = np.array([0.10, 0.06])
 imgs1, imgs2 = [], []
 
 for _ in range(1):
-    rew_list = []
-    cost1_list = []
-    cost2_list = []
     obs = env.reset()
-    env.set_state(np.array([q[0]]), np.array([q[1]]))
-    imgs1.append(env.render("rgb_array"))
-    done = False
-    while not done:
-        act, _ = exp.predict(obs, deterministic=True)
-        obs, rew, done, info = env.step(act)
-        cost = obs @ exp.Q @ obs.T + act*act*exp.R
-        imgs1.append(env.render("rgb_array"))
-        rew_list.append(rew.item())
-        cost1_list.append(cost.item())
-        time.sleep(dt)
-    env.reset()
     done = False
     env.set_state(np.array([q[0]]), np.array([q[1]]))
-    imgs2.append(env.render("rgb_array"))
+    env.render()
     while not done:
-        act, _ = algo.predict(obs, deterministic=True)
+        act, _ = algo.predict(obs, deterministic=False)
         obs, rew, done, info = env.step(act)
-        cost = obs @ exp.Q @ obs.T + act*act*exp.R
-        imgs2.append(env.render("rgb_array"))
-        rew_list.append(rew.item())
-        cost2_list.append(cost.item())
-        time.sleep(dt)
-
-    fig = plt.figure()
-    ax1 = fig.add_subplot(1, 2, 1)
-    ax2 = fig.add_subplot(1, 2, 2)
-    ax1.plot(cost1_list)
-    ax2.plot(cost2_list)
-    plt.show()
-    print(sum(cost1_list), sum(cost2_list))
-    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    width, height, _ = imgs1[0].shape
-    writer1 = cv2.VideoWriter("expert.avi", fourcc, 1/dt, (width, height))
-    writer2 = cv2.VideoWriter("agent.avi", fourcc, 1/dt, (width, height))
-    for img1 in imgs1:
-        img = cv2.cvtColor(img1, cv2.COLOR_RGB2BGR)
-        writer1.write(img)
-    for img2 in imgs2:
-        img = cv2.cvtColor(img2, cv2.COLOR_RGB2BGR)
-        writer2.write(img)
+        env.render()
+        # time.sleep(dt)
