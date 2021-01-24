f12eaf566dabf8ffc4a8c63e60a01144f4d226d9
sw.hong <sw.hong@kaist.ac.kr>
2021-01-24 16:36:17 +0900

    Fix: Made easier draw more figures

--- IRL/IP/test/test_cost.py
+++ IRL/IP/test/test_cost.py
@@ -15,46 +15,44 @@ class ExpertPolicy(LQRPolicy):
         self.B = np.array([[1/I], [0]])
         return self.A, self.B, self.Q, self.R
 
-name = "bar3"
-num = 50
-model_dir = os.path.join("..", "tmp", "model", name)
-costfn = torch.load(model_dir + "/costfn{}.pt".format(num))
-algo = PPO.load(model_dir + "/ppo{}.zip".format(num))
-env = gym.make("IP_custom-v1", n_steps=100)
-exp = ExpertPolicy(env)
+name = "2021-1-24-0-23-35"
+nums = [4]
+for num in nums:
+    model_dir = os.path.join("..", "tmp", "log", name, "model")
+    costfn = torch.load(model_dir + "/costfn%d.pt"%(num)).to('cpu')
+    # algo = PPO.load(model_dir + "/ppo%d.zip"%(num))
+    algo = PPO.load(model_dir + "/extra_ppo.zip".format(name))
+    env = gym.make("IP_custom-v1", n_steps=100)
+    exp = ExpertPolicy(env)
+    dt = env.dt
+    size = 200
+    th, dth = np.meshgrid(np.linspace(-0.3, 0.3, size), np.linspace(-0.3, 0.3, size))
+    pact = np.zeros((size, size), dtype=np.float64)
+    act = np.zeros((size, size), dtype=np.float64)
+    cost_agt = np.zeros(th.shape)
+    cost_exp = np.zeros(th.shape)
 
-dt = env.dt
+    for i in range(th.shape[0]):
+        for j in range(th.shape[1]):
+            pact[i][j], _ = algo.predict(np.append(th[i][j], dth[i][j]), deterministic=True)
+            act[i][j], _ = exp.predict(np.append(th[i][j], dth[i][j]), deterministic=True)
+            cost_exp[i][j] = np.array([th[i][j], dth[i][j]])@exp.Q@np.array([th[i][j], dth[i][j]]).T+act[i][j]*act[i][j]*exp.R*exp.gear**2
+            inp = torch.Tensor((th[i][j], dth[i][j], pact[i][j])).double()
+            cost_agt[i][j] = costfn(inp).item()
 
-th, dth = np.meshgrid(np.linspace(-0.5, 0.5, 100), np.linspace(-0.5, 0.5, 100))
-pact = np.zeros((100, 100), dtype=np.float64)
-act = -exp.P*th - exp.D*dth
-cost_agt = np.zeros(th.shape)
-cost_exp = np.zeros(th.shape)
-
-for i in range(th.shape[0]):
-    for j in range(th.shape[1]):
-        pact[i][j], _ = algo.predict(np.append(th[i][j], dth[i][j]), deterministic=True)
-        cost_exp[i][j] = np.array([th[i][j], dth[i][j]])@exp.Q@np.array([th[i][j], dth[i][j]]).T+act[i][j]*act[i][j]*exp.R
-        inp = torch.Tensor([th[i][j], dth[i][j], act[i][j]]).double()
-        cost_agt[i][j] = costfn(inp).item()
-
-cost_agt /= np.amax(cost_agt)
-cost_exp /= np.amax(cost_exp)
-fig = plt.figure()
-ax1 = fig.add_subplot(2, 2, 1, projection='3d')
-ax2 = fig.add_subplot(2, 2, 2, projection='3d')
-ax3 = fig.add_subplot(2, 2, 3, projection='3d')
-ax4 = fig.add_subplot(2, 2, 4, projection='3d')
-surf1 = ax1.plot_surface(th, dth, cost_agt, cmap=cm.coolwarm)
-surf2 = ax2.plot_surface(th, dth, cost_exp, cmap=cm.coolwarm)
-surf3 = ax3.plot_surface(th, dth, pact, cmap=cm.coolwarm)
-surf4 = ax4.plot_surface(th, dth, act, cmap=cm.coolwarm)
-fig.colorbar(surf1, ax=ax1)
-fig.colorbar(surf2, ax=ax2)
-fig.colorbar(surf3, ax=ax3)
-fig.colorbar(surf4, ax=ax4)
-ax1.view_init(azim=0, elev=90)
-ax2.view_init(azim=0, elev=90)
-ax3.view_init(azim=0, elev=90)
-ax4.view_init(azim=0, elev=90)
-plt.show()
+    cost_agt /= np.amax(cost_agt)
+    cost_exp /= np.amax(cost_exp)
+    title_list = ["norm_cost", "norm_cost", "abs_action", "abs_action"]
+    yval_list = [cost_agt, cost_exp, np.abs(pact), np.abs(act)]
+    xlabel, ylabel = "theta(rad)", "dtheta(rad/s)"
+    max_list = [1.0, 1.0, 0.3, 0.3]
+    min_list = [0.0, 0.0, 0.0, 0.0]
+    fig = plt.figure()
+    for i in range(4):
+        ax = fig.add_subplot(2, 2, (i+1))
+        surf = ax.pcolor(th, dth, yval_list[i], cmap=cm.coolwarm, shading='auto', vmax=max_list[i], vmin=min_list[i])
+        clb = fig.colorbar(surf, ax=ax)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        clb.ax.set_title(title_list[i])
+    plt.show()
