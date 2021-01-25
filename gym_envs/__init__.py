e96fd84e8875697990c59beebfbb672b62f6b0fa
sw.hong <sw.hong@kaist.ac.kr>
2021-01-25 14:09:01 +0900

    Fix: Adjust the number of environment version

--- gym_envs/__init__.py
+++ gym_envs/__init__.py
@@ -9,9 +9,10 @@ register(
     id="CartPoleContTest-v0",
     entry_point='gym_envs.envs:CartPoleContTestEnv'
 )
-
+# expert: v0, agent: v1
+# v2 environment is not used yet
 register(
-    id='IP_custom-v0',
+    id='IP_custom-v2',
     entry_point='gym_envs.envs:IP_custom',
     )
 
@@ -21,11 +22,16 @@ register(
 )
 
 register(
-    id='IP_custom-v2',
-    entry_point='gym_envs.envs:IP_custom_PD'
+    id='IP_custom-v0',
+    entry_point='gym_envs.envs:IP_custom_exp'
 )
 
 register(
-    id='IDP_custom-v0',
+    id='IDP_custom-v1',
     entry_point='gym_envs.envs:IDP_custom',
+    )
+
+register(
+    id='IDP_custom-v0',
+    entry_point='gym_envs.envs:IDP_custom_exp',
     )
