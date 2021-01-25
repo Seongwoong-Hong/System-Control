8b89f15e2c2ad6b4d875e50af8d3b62976d8a9f9
sw.hong <sw.hong@kaist.ac.kr>
2021-01-25 17:38:25 +0900

    refactor: Change according to the new script "project_policies"

--- IRL/CollectExperts.py
+++ IRL/CollectExperts.py
@@ -1,45 +1,7 @@
 import gym, gym_envs
-import numpy as np
 from imitation.data import rollout, types
 from stable_baselines3.common.vec_env import DummyVecEnv
-from algo.torch.OptCont.policies import LQRPolicy
-
-# for the IP environment
-class IPPolicy(LQRPolicy):
-    def _build_env(self):
-        m, h, I, g = 5.0, 0.5, 1.667, 9.81
-        Q = np.array([[1, 0], [0, 1]])
-        R = 0.001*np.array([[1]])
-        A = np.array([[0, 1],
-                      [m*g*h/I, 0]])
-        B = np.array([[0], [1/I]])
-        return A, B, Q, R
-
-# for the IDP environment
-class IDPPolicy(LQRPolicy):
-    def _build_env(self):
-        m1, m2, h1, h2, I1, I2, g = 5.0, 5.0, 0.5, 0.5, 1.667, 1.667, 9.81
-        Q = np.array([[1, 0, 0, 0],
-                      [0, 1, 0, 0],
-                      [0, 0, 0, 0],
-                      [0, 0, 0, 0]])
-        R = 0.0001*np.array([[1, 0],
-                             [0, 1]])
-        A = np.array([[0, 0, 1, 0],
-                      [0, 0, 0, 1],
-                      [m1*g*h1/I1, 0, 0, 0],
-                      [0, m2*g*h2/I2, 0, 0]])
-        B = np.array([[0, 0], [0, 0],
-                      [1/I1, -1/I1], [0, 1/I2]])
-        return A, B, Q, R
-
-def def_policy(env_type, env):
-    if env_type == "IP":
-        return IPPolicy(env)
-    elif env_type == "IDP":
-        return IDPPolicy(env)
-    else:
-        raise NameError("Not defined policy name")
+from IRL.project_policies import def_policy
 
 if __name__ == "__main__":
     n_steps, n_episodes = 100, 30
