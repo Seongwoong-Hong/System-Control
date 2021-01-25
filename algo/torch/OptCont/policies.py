82eecec6b1a991fa9d43da2078b5523250dde875
sw.hong <sw.hong@kaist.ac.kr>
2021-01-25 13:46:38 +0900

    Fix: Change for getting gains in the multiple actuator environment successfully

--- algo/torch/OptCont/policies.py
+++ algo/torch/OptCont/policies.py
@@ -13,22 +13,27 @@ class LQRPolicy(BasePolicy):
             action_space)
 
         self.gear = env.model.actuator_gear[0, 0]
-        self.P, self.D = self._get_gains()
+        self.K = self._get_gains()
 
-    def _get_gains(self):
+    def _get_gains(self) -> th.Tensor:
         A, B, Q, R = self._build_env()
         X = linalg.solve_continuous_are(A, B, Q, R)
-        K = (1/R * (B.T @ X)).reshape(-1)
-        return K[1], K[0]
+        if R.shape[0] >= 2:
+            K = (np.linalg.inv(R) @ (B.T @ X))
+        else:
+            K = (1/R * (B.T @ X)).reshape(-1)
+        return th.from_numpy(K)
 
-    def _build_env(self):
+    def _build_env(self) -> np.array:
+        # returns A, B, Q, R
+        # Their types are all numpy array even though they are 1-dim value.
         raise NotImplementedError
 
     def forward(self):
         return None
 
     def _predict(self, observation: th.Tensor, deterministic: bool = False) -> th.Tensor:
-        action = -1/self.gear * (self.P*observation[0][1] + self.D*observation[0][0]).reshape(1, 1)
+        action = -1/self.gear * (self.K @ observation.T).reshape(1, -1)
         if deterministic:
             return action
         else:
