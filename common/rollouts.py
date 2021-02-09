6968a796216ce1b34fe6fbcdd5de48e5410faa51
sw.hong <sw.hong@kaist.ac.kr>
2021-02-09 15:24:37 +0900

    style: Change the types of the result for faster calculation during cost learning

--- common/rollouts.py
+++ common/rollouts.py
@@ -2,20 +2,24 @@ import copy
 import torch
 from typing import Sequence
 
-from imitation.data import rollout, types
+from imitation.data import rollout
 
 
 def get_trajectories_probs(
         trajectories,
         policy,
-) -> Sequence[types.TransitionsWithRew]:
+) -> Sequence[torch.Tensor]:
     transitions = []
     for traj in trajectories:
         trans = copy.deepcopy(rollout.flatten_trajectories_with_rew([traj]))
-        for i in range(len(trans)):
-            obs = torch.from_numpy(trans[i]['obs'].reshape(1, -1)).to(policy.device)
-            acts = torch.from_numpy(trans[i]['acts'].reshape(1, -1)).to(policy.device)
+        obs = torch.from_numpy(trans[0]['obs'].reshape(1, -1)).to(policy.device)
+        acts = torch.from_numpy(trans[0]['acts'].reshape(1, -1)).to(policy.device)
+        log_probs = policy.get_log_prob_from_act(obs, acts)
+        concats = torch.cat((obs, acts, log_probs.reshape(-1, 1)), dim=1)
+        for i in range(len(trans)-1):
+            obs = torch.from_numpy(trans[i+1]['obs'].reshape(1, -1)).to(policy.device)
+            acts = torch.from_numpy(trans[i+1]['acts'].reshape(1, -1)).to(policy.device)
             log_probs = policy.get_log_prob_from_act(obs, acts)
-            trans[i]['infos']['log_probs'], trans[i]['infos']['rwinp'] = log_probs, torch.cat((obs, acts), dim=1).reshape(-1)
-        transitions.append(trans)
+            concats = torch.cat((concats, torch.cat((obs, acts, log_probs.reshape(-1, 1)), dim=1)), dim=0)
+        transitions += [concats]
     return transitions
