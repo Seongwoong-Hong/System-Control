3497526956bb1a7bc10dfe1f12561570e95a8848
sw.hong <sw.hong@kaist.ac.kr>
2021-04-02 14:14:56 +0900

    style: Changes for logging parameters and minor modifications

--- IRL/AIRL.py
+++ IRL/AIRL.py
@@ -1,8 +1,10 @@
 import os
+import shutil
 import pickle
 import pathlib
 import gym_envs
 import datetime
+import time
 
 from stable_baselines3.common.vec_env import DummyVecEnv
 from imitation.algorithms import adversarial
@@ -10,33 +12,33 @@ from imitation.data import rollout
 from imitation.util import logger
 
 from IRL.project_policies import def_policy
+from common.callbacks import VideoCallback
 from mujoco_py import GlfwContext
 from scipy import io
 
 
 if __name__ == "__main__":
-    env_type = "HPC"
+    env_type = "IDP"
     algo_type = "ppo"
-    num = 2
     env_id = "{}_custom-v0".format(env_type)
     n_steps = 600
     device = "cpu"
     current_path = os.path.dirname(__file__)
     GlfwContext(offscreen=True)
     sub = "sub01"
-    expert_dir = os.path.join(current_path, "demos", env_type, sub + ".pkl")
+    expert_dir = os.path.join(current_path, "demos", env_type, "expert" + ".pkl")
     pltqs = []
 
     with open(expert_dir, "rb") as f:
         expert_trajs = pickle.load(f)
-        learner_trajs = []
+
     transitions = rollout.flatten_trajectories(expert_trajs)
 
-    for i in range(35):
-        file = os.path.join(current_path, "demos", env_type, sub, sub + "i%d.mat" % (i + 1))
-        pltqs += [io.loadmat(file)['pltq']]
+    # for i in range(35):
+    #     file = os.path.join(current_path, "demos", env_type, sub, sub + "i%d.mat" % (i + 1))
+    #     pltqs += [io.loadmat(file)['pltq']]
 
-    env = DummyVecEnv([lambda: gym_envs.make(env_id, n_steps=n_steps, pltqs=pltqs) for i in range(10)])
+    env = DummyVecEnv([lambda: gym_envs.make(env_id, n_steps=n_steps) for _ in range(10)])
 
     algo = def_policy(algo_type, env, device=device, log_dir=None, verbose=1)
 
@@ -46,23 +48,42 @@ if __name__ == "__main__":
     if not os.path.isdir(log_dir):
         os.mkdir(log_dir)
     name = "/%s-%s-%s-%s-%s-%s" % (now.year, now.month, now.day, now.hour, now.minute, now.second)
+    # name = "/14"
     log_dir += name
     if not os.path.isdir(log_dir):
         os.mkdir(log_dir)
     print(f"All Tensorboards and logging are being written inside {log_dir}/.")
 
+    model_dir = os.path.join(log_dir, "model")
+    if not os.path.isdir(model_dir):
+        os.mkdir(model_dir)
+    # Copy used file to logging folder
+    shutil.copy(os.path.abspath(current_path + "/../gym_envs/envs/{}_custom_exp.py".format(env_type)), model_dir)
+    shutil.copy(os.path.abspath(__file__), model_dir)
+    shutil.copy(os.path.abspath(current_path + "/project_policies.py"), model_dir)
+
     logger.configure(log_dir, format_strs=["stdout", "log", "csv", "tensorboard"])
+
+    video_recorder = VideoCallback(gym_envs.make(env_id, n_steps=n_steps),
+                                   n_eval_episodes=5,
+                                   render_freq=10)
+
     airl_trainer = adversarial.AIRL(
         env,
         expert_data=transitions,
-        expert_batch_size=15,
+        expert_batch_size=8,
         gen_algo=algo,
+        discrim_kwargs={"entropy_weight": 0.4},
+        disc_opt_kwargs={"lr": 1e-3}
     )
-    airl_trainer.train(total_timesteps=10240000)
+    airl_trainer.train(total_timesteps=6000000)
 
-    path = log_dir + "result.pkl"
-    p = pathlib.Path(path)
+    disc_path = model_dir + "/discrim.pkl"
+    p = pathlib.Path(disc_path)
     p.parent.mkdir(parents=True, exist_ok=True)
-    with open(path + ".tmp", "wb") as f:
-        pickle.dump(airl_trainer, f)
-    os.replace(path + ".tmp", path)
+    with open(disc_path + ".tmp", "wb") as f:
+        pickle.dump(airl_trainer.discrim, f)
+    os.replace(disc_path + ".tmp", disc_path)
+
+    gen_path = model_dir + "/gen"
+    airl_trainer.gen_algo.save(gen_path)
