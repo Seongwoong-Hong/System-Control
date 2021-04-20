08b587c7154c956ecc3b1e0447c250e24105065b
sw.hong <sw.hong@kaist.ac.kr>
2021-04-20 15:39:09 +0900

    fix: Redefine the environment for every iteration

--- IRL/RunAnalysis.py
+++ IRL/RunAnalysis.py
@@ -1,42 +1,22 @@
-import gym_envs
 import os
+import gym_envs
 import numpy as np
 
-from copy import deepcopy
-from mujoco_py import GlfwContext
 from scipy import io
 from matplotlib import pyplot as plt
 
 from algo.torch.ppo import PPO
 
-
 if __name__ == "__main__":
     env_type = "IDP"
     algo_type = "ppo"
-    env_id = "{}_custom-v1".format(env_type)
+    env_id = "{}_custom-v0".format(env_type)
     n_steps = 600
     device = "cpu"
     current_path = os.path.dirname(__file__)
-    GlfwContext(offscreen=True)
     sub = "sub01"
     expert_dir = os.path.join(current_path, "demos", env_type, sub + ".pkl")
-    pltqs = []
-
-    if env_type == "HPC":
-        for i in range(35):
-            file = os.path.join(current_path, "demos", env_type, sub, sub + "i%d.mat" % (i + 1))
-            pltqs += [io.loadmat(file)['pltq']]
-        env = gym_envs.make("{}_custom-v1".format(env_type), n_steps=None, pltqs=pltqs)
-    else:
-        env = gym_envs.make("{}_custom-v1".format(env_type))
-
     ana_dir = os.path.join(current_path, "tmp", "log", env_type, algo_type, "AIRL_hype_tune")
-
-    init_obs = env.reset().reshape(1, -1)
-    init_obs = np.append(init_obs, env.reset().reshape(1, -1), 0)
-    init_obs = np.append(init_obs, env.reset().reshape(1, -1), 0)
-    init_obs = np.append(init_obs, env.reset().reshape(1, -1), 0)
-    init_obs = np.append(init_obs, env.reset().reshape(1, -1), 0)
 
     Q = np.array([[1, 0, 0, 0],
                   [0, 1, 0, 0],
@@ -48,20 +28,29 @@ if __name__ == "__main__":
     num_list, rew_list = [], []
     num = 0
     while os.path.isdir(ana_dir + "/{}".format(num)):
+        pltqs = []
+        if env_type == "HPC":
+            for i in [0, 5, 10, 15, 20, 25, 30]:
+                file = os.path.join(current_path, "demos", env_type, sub, sub + "i%d.mat" % (i + 1))
+                pltqs += [io.loadmat(file)['pltq']]
+            env = gym_envs.make(env_id, n_steps=None, pltqs=pltqs)
+        else:
+            env = gym_envs.make(env_id, n_steps=None)
         policy = PPO.load(ana_dir + "/{}/model/gen.zip".format(num))
         rew = 0
-        for init_ob in init_obs:
-            env.reset()
-            env.set_state(init_ob[:2], init_ob[2:])
-            obs = deepcopy(init_ob)
+        for _ in range(7):
+            obs = env.reset()
             for _ in range(600):
                 act, _ = policy.predict(obs, deterministic=True)
-                rew += obs @ Q @ obs + act @ R @ act
+                rew += obs[:4] @ Q @ obs[:4] + act @ R @ act
                 obs, _, _, _ = env.step(act)
-        rew_list.append(5/rew)
+        rew_list.append(7 / rew)
         num_list.append(num)
         num += 1
-
-    plt.plot(num_list, rew_list)
-    plt.show()
-    env.close()
+        env.close()
+    pre = 0
+    while pre < len(num_list):
+        post = min(len(num_list), pre+25)
+        plt.plot(num_list[pre:post], rew_list[pre:post])
+        plt.show()
+        pre = post
