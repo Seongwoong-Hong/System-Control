8ff70f63a205652c437c126923c850c660ce2543
sw.hong <sw.hong@kaist.ac.kr>
2021-04-21 19:17:45 +0900

    feat: Add the method for getting the 'order' attribute

--- gym_envs/envs/HPC_custom.py
+++ gym_envs/envs/HPC_custom.py
@@ -20,6 +20,7 @@ class IDPHuman(mujoco_env.MujocoEnv, utils.EzPickle):
         if bsp is not None:
             self.bsp = bsp
         self.traj_len = 0
+        self._order = 0
 
     def step(self, action: np.ndarray):
         self.do_simulation(action + self.plt_torque, self.frame_skip)
@@ -27,13 +28,14 @@ class IDPHuman(mujoco_env.MujocoEnv, utils.EzPickle):
         r = - (ob[0] ** 2 + ob[1] ** 2 + 1e-6 * action @ np.eye(2, 2) @ action.T)
         done = False
         info = {}
+        self.traj_len += 1
         if self.n_steps is None:
             pass
         elif (self.traj_len + 1) == self.n_steps:
             done = True
             info = {"terminal observation": ob}
             self.traj_len = 0
-        self.traj_len += 1
+            self._order += 1
         return ob, r, done, info
 
     def _get_obs(self):
@@ -51,6 +53,10 @@ class IDPHuman(mujoco_env.MujocoEnv, utils.EzPickle):
     def set_state(self, qpos, qvel):
         super().set_state(qpos, qvel)
         self._set_pltqs()
+
+    @property
+    def order(self):
+        return self._order % len(self._pltqs)
 
     @property
     def plt_torque(self):
@@ -87,17 +93,10 @@ class IDPHumanExp(IDPHuman):
     def __init__(self, n_steps=None, bsp=None, pltqs=None):
         super().__init__(n_steps=n_steps, bsp=bsp)
         self._pltqs = pltqs
-        self.order = 0
         self._set_pltqs()
-
-    def reset_model(self):
-        self.traj_len = 0
-        self.order += 1
-        self.set_state(self.init_qpos, self.init_qvel)
-        return self._get_obs()
 
     def _set_pltqs(self):
         if self._pltqs is not None:
-            self._pltq = self._pltqs[self.order % len(self._pltqs)] / self.model.actuator_gear[0, 0]
+            self._pltq = self._pltqs[self.order] / self.model.actuator_gear[0, 0]
         else:
             self._pltq = None
