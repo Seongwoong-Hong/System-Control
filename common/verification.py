0f2e95dbe383206c0084d7f118b323469f8fef94
sw.hong <sw.hong@kaist.ac.kr>
2021-04-22 21:18:20 +0900

    feat: Add a new objective that calculates the cost and draws the cost map

--- common/verification.py
+++ common/verification.py
@@ -2,9 +2,11 @@ import os
 import time
 import cv2
 import numpy as np
+import torch as th
 
 from copy import deepcopy
 from mujoco_py import GlfwContext
+from matplotlib import pyplot as plt
 
 
 def video_record(imgs, filename, dt):
@@ -42,3 +44,41 @@ def verify_policy(environment, policy, render="human"):
         time.sleep(environment.dt)
         imgs.append(img)
     return action_list, ob_list, imgs
+
+
+class CostMap:
+    def __init__(self, agent, expt=None):
+        self.agent = agent
+        self.expt = expt
+
+    @staticmethod
+    def cal_cost(agents):
+        inputs = []
+        for agent in agents:
+            transitions = agent['transitions']
+            cost_fn = agent['cost_fn']
+            costs = []
+            for tran in transitions:
+                cost = 0
+                for t in range(len(tran)):
+                    obs = th.from_numpy(tran.obs[np.newaxis, t, :])
+                    act = th.from_numpy(tran.acts[np.newaxis, t, :])
+                    next_obs = th.from_numpy(tran.next_obs[np.newaxis, t, :])
+                    cost += cost_fn(obs, act, next_obs, tran.dones[t])
+                costs.append(cost)
+            orders = agent['orders']
+            inputs.append([orders, costs])
+        return inputs
+
+    @staticmethod
+    def draw_costmap(inputs):
+        fig = plt.figure()
+        titles = ["agent", "expert"]
+        for i, (orders, costs) in enumerate(inputs):
+            ax = fig.add_subplot(len(inputs), 1, i+1)
+            ax.plot(orders, costs)
+            ax.set_xlabel("order")
+            ax.set_ylabel("cost")
+            ax.set_title(titles[i])
+        plt.show()
+        return fig
