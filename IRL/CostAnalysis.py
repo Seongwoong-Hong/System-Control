c0fc405e0bd046f8b522b942e5abfae283f8ad66
sw.hong <sw.hong@kaist.ac.kr>
2021-04-28 20:24:17 +0900

    refactor: Convert the reward function to the cost function

--- IRL/CostAnalysis.py
+++ IRL/CostAnalysis.py
@@ -6,17 +6,20 @@ from common.util import make_env
 from common.verification import CostMap
 
 if __name__ == "__main__":
-    env_type = "IDP"
+    env_type = "HPC"
     algo_type = "ppo"
     device = "cpu"
     current_path = os.path.dirname(__file__)
     env = make_env(f"{env_type}_custom-v0", use_vec_env=False, n_steps=600, sub="sub01")
-    ana_dir = os.path.join(current_path, "tmp", "log", env_type, algo_type, "AIRL_hype_tune")
-
-    model_dir = os.path.join(ana_dir, "104", "model")
+    expt_env = make_env(f"{env_type}_custom-v0", use_vec_env=False, n_steps=600, sub="sub01")
+    ana_dir = os.path.join(current_path, "tmp", "log", env_type, algo_type, "AIRL_test")
+    model_dir = os.path.join(ana_dir, "1", "model")
     with open(model_dir + "/discrim.pkl", "rb") as f:
-        disc = pickle.load(f)
-    cost_fn = disc.reward_net.base_reward_net.double()
+        disc = pickle.load(f).double()
+
+    def cost_fn(*args, **kwargs):
+        data = disc.reward_net.base_reward_net(*args, **kwargs)
+        return -data
     agent = PPO.load(model_dir + "/gen.zip")
-    # agent = def_policy(env_type, env)
-    cost_map = CostMap(env, cost_fn, agent)
+    expt = def_policy(env_type, expt_env)
+    cost_map = CostMap(cost_fn, env, agent, expt_env, expt)
