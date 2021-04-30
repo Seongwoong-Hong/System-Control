ba5203d98c119f247097e22cc3f973a2b7adf1c6
sw.hong <sw.hong@kaist.ac.kr>
2021-04-30 18:17:07 +0900

    refactor: Apply a new util function

--- IRL/RewardAnalysis.py
+++ IRL/RewardAnalysis.py
@@ -6,15 +6,15 @@ from matplotlib import pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 
 from algo.torch.ppo import PPO
-from common.util import make_env
+from common.util import make_env, write_analyzed_result
 
 if __name__ == "__main__":
-    env_type = "HPC"
+    env_type = "IDP"
     algo_type = "ppo"
     device = "cpu"
     current_path = os.path.dirname(__file__)
     sub = "sub01"
-    ana_dir = os.path.join(current_path, "tmp", "log", env_type, algo_type, "AIRL_test1")
+    ana_dir = os.path.join(current_path, "tmp", "log", env_type, algo_type, "AIRL_Single_test")
 
     pltqs = []
     test_len = 5
@@ -30,13 +30,14 @@ if __name__ == "__main__":
     num = 0
     result_dict = {}
 
+    Q = np.array([[1, 0, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]])
+    R = 1e-6 * np.array([[1, 0],
+                         [0, 1]])
+
     def ana_fnc():
-        Q = np.array([[1, 0, 0, 0],
-                      [0, 1, 0, 0],
-                      [0, 0, 0, 0],
-                      [0, 0, 0, 0]])
-        R = 1e-6 * np.array([[1, 0],
-                             [0, 1]])
         param = 0
         for _ in range(test_len):
             obs = env.reset()
@@ -45,38 +46,30 @@ if __name__ == "__main__":
                 act, _ = policy.predict(obs, deterministic=True)
                 param += obs[:4] @ Q @ obs[:4] + act @ R @ act
                 obs, _, done, _ = env.step(act)
-        return param
+        return {'cost': param}
+
     while os.path.isfile(ana_dir + f"/{num}/model/gen.zip"):
         policy = PPO.load(ana_dir + f"/{num}/model/gen.zip")
         file = ana_dir + f"/{num}/model/hyper_parameters.txt"
-        if not os.path.isfile(ana_dir + f"/{num}/model/result.txt"):
-            cost = ana_fnc()
-            f = open(ana_dir + f"/{num}/model/result.txt", "w")
-            f.write(f"cost: {cost}\n")
-            f.close()
+        write_analyzed_result(ana_fnc, ana_dir, iter_name=num)
         f = open(ana_dir + f"/{num}/model/result.txt", "r")
-        saved_value = None
+        cost = None
         for line in f.readlines():
             if "cost" in line:
                 cost = float(line[line.find(":") + 1:-1])
-        assert saved_value is None, "Can't find the cost value"
-        rew_list.append(test_len / cost)
+        assert cost is not None, "Can't find the cost value"
+        rew = test_len / cost
+        rew_list.append(rew)
         num_list.append(num)
         num += 1
         f = open(file, "r")
         for line in f.readlines():
             param_name, param_value = line[:line.find(":")], float(line[line.find(":") + 1:-1])
             if param_name in result_dict:
-                result_dict[param_name].append([param_value, test_len / cost])
+                result_dict[param_name].append([param_value, rew])
             else:
-                result_dict[param_name] = [[param_value, test_len / cost]]
-        env.close()
-    pre = 0
-    while pre < len(num_list):
-        post = min(len(num_list), pre+25)
-        plt.plot(num_list[pre:post], rew_list[pre:post])
-        plt.show()
-        pre = post
+                result_dict[param_name] = [[param_value, rew]]
+    env.close()
     for key, value in result_dict.items():
         fig = plt.figure()
         ax = fig.subplots()
