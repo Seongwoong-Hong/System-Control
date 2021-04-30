160da8e45c600f102b8ca5173e77a369ae591bdc
sw.hong <sw.hong@kaist.ac.kr>
2021-04-30 18:16:03 +0900

    feat: Add a function that writes a text file contains analyzed results

--- common/util.py
+++ common/util.py
@@ -1,4 +1,5 @@
 import os
+import os.path as p
 import gym_envs
 
 from stable_baselines3.common.vec_env import DummyVecEnv
@@ -12,22 +13,50 @@ def make_env(env_name, use_vec_env=True, num_envs=10, sub=None, **kwargs):
             pltqs = []
             i = 0
             while True:
-                file = os.path.join("demos", env_type, sub, sub + "i%d.mat" % (i + 1))
-                if not os.path.isfile(file):
+                file = p.join("demos", env_type, sub, sub + "i%d.mat" % (i + 1))
+                if not p.isfile(file):
                     break
                 pltqs += [io.loadmat(file)['pltq']]
                 i += 1
+            kwargs['pltqs'] = pltqs
         if use_vec_env:
             env = DummyVecEnv([lambda: gym_envs.make(env_name, **kwargs) for _ in range(num_envs)])
         else:
             env = gym_envs.make(env_name, **kwargs)
-    else:
+    elif env_type == "IDP":
         kwargs.pop('pltqs', None)
         if use_vec_env:
             env = DummyVecEnv([lambda: gym_envs.make(env_name, **kwargs) for _ in range(num_envs)])
         else:
             env = gym_envs.make(env_name, **kwargs)
+    else:
+        raise NameError('Unknown environment type')
     return env
+
+
+def write_analyzed_result(ana_fn,
+                          ana_dir,
+                          iter_name=None,
+                          result_path: str = "/model/result.txt",
+                          verbose=0):
+    if iter_name is not None:
+        filename = ana_dir + f"/{iter_name}" + result_path
+        assert p.isdir(p.abspath(p.join(filename, p.pardir))), "Directory doesn't exist"
+    else:
+        filename = ana_dir + result_path
+        assert p.isdir(p.abspath(p.join(filename, p.pardir))), "Directory doesn't exist"
+    if not p.isfile(filename):
+        ana_dict = ana_fn()
+        f = open(filename + ".tmp", "w")
+        for key, value in ana_dict.items():
+            f.write(f"{key}: {value}\n")
+        f.close()
+        os.replace(filename + ".tmp", filename)
+        if verbose == 1:
+            print("The result file saved successfully")
+    else:
+        if verbose == 1:
+            print("A result file already exists")
 
 
 def remove_analyzed_result(ana_dir, entire=True, folder_num=None):
@@ -35,10 +64,10 @@ def remove_analyzed_result(ana_dir, entire=True, folder_num=None):
         folder_num = 0
         while True:
             file = ana_dir + f"/{folder_num}/model/result.txt"
-            if not os.path.isdir(os.path.dirname(file)):
+            if not p.isdir(p.dirname(file)):
                 print(f"Break at the folder #{folder_num}")
                 break
-            if not os.path.isfile(file):
+            if not p.isfile(file):
                 print(f"Pass the folder #{folder_num}")
                 folder_num += 1
                 continue
@@ -47,6 +76,6 @@ def remove_analyzed_result(ana_dir, entire=True, folder_num=None):
     else:
         assert folder_num is not None, "The folder number was not specified"
         file = ana_dir + f"/{folder_num}/model/result.txt"
-        if os.path.isfile(file):
+        if p.isfile(file):
             os.remove(file)
             print(f"The result file in the folder #{folder_num} is removed successfully")
