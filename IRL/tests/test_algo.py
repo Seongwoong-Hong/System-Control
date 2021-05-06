3d2b3346f6d14783cc354dbd2e898df870d3c480
sw.hong <sw.hong@kaist.ac.kr>
2021-05-06 21:10:43 +0900

    test: Add a new test for some mujoco default envrionments

--- IRL/tests/test_algo.py
+++ IRL/tests/test_algo.py
@@ -27,9 +27,9 @@ def test_hpc_learned_policy(env):
 
 
 def test_idp_learned_policy():
-    env = gym_envs.make("IDP_custom-v0", n_steps=600)
-    # name = "IDP/ppo/AIRL_test/" + "1"
-    name = "IDP/ppo/AIRL/2021-4-29-23-5-4"
+    env = gym_envs.make("IDP_custom-v2", n_steps=600)
+    name = "IDP/ppo/AIRL_easy/" + "16"
+    # name = "IDP/ppo/forward"
     model_dir = os.path.join("..", "tmp", "log", name, "model")
     algo = PPO.load(model_dir + "/gen.zip")
     a_list, o_list, _ = verify_policy(env, algo)
@@ -39,3 +39,15 @@ def test_idp_policy():
     env = gym_envs.make("IDP_custom-v0", n_steps=600)
     algo = def_policy("IDP", env)
     _, _, _ = verify_policy(env, algo)
+
+
+def test_mujoco_policy():
+    import gym
+    name = "InvertedDoublePendulum"
+    env = gym.make(f"{name}-v2")
+    # model_dir = os.path.join("..", "tmp", "log", "mujoco_envs", "ppo", name, "sample", "checkpoints", "final", "gen_policy")
+    model_dir = os.path.join("..", "tmp", "log", "mujoco_envs", "ppo", name+"2", "17", "model")
+    # model_dir = os.path.join("..", "..", "RL", "mujoco_envs", "tmp", "log", name, "ppo")
+    algo = PPO.load(model_dir + "/gen.zip")
+    for _ in range(10):
+        verify_policy(env, algo, deterministic=False)
