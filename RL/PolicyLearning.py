d00fee4efd84915cd66e6064bf4e66e1773bfac7
sw.hong <sw.hong@kaist.ac.kr>
2021-05-12 16:01:37 +0900

    feat: Add policy defining function

--- RL/PolicyLearning.py
+++ RL/PolicyLearning.py
@@ -1,50 +1,34 @@
-import gym
 import os
 
 from stable_baselines3.common import callbacks
 from imitation.policies import serialize
 
-from algo.torch.ppo import PPO, MlpPolicy
 from common.callbacks import VideoCallback
 from common.util import create_path, make_env
+from RL.project_policies import def_policy
+
 from mujoco_py import GlfwContext
 
 
 if __name__ == "__main__":
-    env_type = "Pendulum"
-    algo_type = "ppo"
-    name = f"{env_type}/{algo_type}"
-    env_id = "Pendulum-v0"
+    env_type = "IP"
+    algo_type = "sac"
+    name = "IP_custom"
     device = "cpu"
+    env_id = f"{name}-v2"
+    env = make_env(env_id, use_vec_env=False, num_envs=8, n_steps=600)
     current_path = os.path.dirname(__file__)
-    log_dir = os.path.join(current_path, "tmp", "log", name)
+    log_dir = os.path.join(current_path, env_type, "tmp", "log", name, algo_type)
     create_path(dirname=log_dir)
     GlfwContext(offscreen=True)
-    env = make_env(env_id, num_envs=8)
-    algo = PPO(MlpPolicy,
-               env=env,
-               n_steps=256,
-               batch_size=64,
-               gamma=0.975,
-               gae_lambda=0.95,
-               learning_rate=3e-4,
-               ent_coef=0.0,
-               n_epochs=2,
-               ent_schedule=1.0,
-               clip_range=0.2,
-               verbose=1,
-               device=device,
-               tensorboard_log=log_dir,
-               policy_kwargs={'log_std_range': [-5, 5],
-                              'net_arch': [{'pi': [32, 32], 'vf': [32, 32]}]},
-               )
+    algo = def_policy(algo_type, env, device=device, log_dir=log_dir, verbose=1)
     n = 1
     while os.path.isdir(log_dir + f"/extra_{n}"):
         n += 1
     create_path(log_dir + f"/policies_{n}")
-    video_recorder = VideoCallback(gym.make(env_id),
+    video_recorder = VideoCallback(make_env(env_id, use_vec_env=False, n_steps=600),
                                    n_eval_episodes=5,
-                                   render_freq=int(1e5))
+                                   render_freq=int(5e5))
     save_policy_callback = serialize.SavePolicyCallback(log_dir + f"/policies_{n}", None)
     save_policy_callback = callbacks.EveryNTimesteps(int(5e5), save_policy_callback)
     callback_list = callbacks.CallbackList([video_recorder, save_policy_callback])
