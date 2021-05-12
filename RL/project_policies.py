64b7c57ceb9ec1b0d34e2b8ac359e07ffb7aebbc
sw.hong <sw.hong@kaist.ac.kr>
2021-05-12 17:37:39 +0900

    feat: project policies for RL works

---
+++ RL/project_policies.py
@@ -0,0 +1,47 @@
+from algo.torch.ppo import PPO
+from algo.torch.sac import SAC
+
+
+def def_policy(algo_type, env, device='cpu', log_dir=None, verbose=0, **kwargs):
+    if algo_type == "ppo":
+        from algo.torch.ppo import MlpPolicy
+        return PPO(MlpPolicy,
+                   env=env,
+                   n_steps=256,
+                   batch_size=64,
+                   gamma=0.99,
+                   gae_lambda=0.95,
+                   learning_rate=3e-4,
+                   ent_coef=0.0,
+                   n_epochs=10,
+                   ent_schedule=1.0,
+                   clip_range=0.2,
+                   verbose=verbose,
+                   device=device,
+                   tensorboard_log=log_dir,
+                   policy_kwargs={'log_std_range': [-5, 5],
+                                  'net_arch': [{'pi': [32, 32], 'vf': [32, 32]}],
+                                  },
+                   **kwargs,
+                   )
+
+    elif algo_type == "sac":
+        from algo.torch.sac import MlpPolicy
+        return SAC(MlpPolicy,
+                   env=env,
+                   buffer_size=int(1e6),
+                   batch_size=256,
+                   learning_starts=100,
+                   train_freq=1,
+                   n_episodes_rollout=-1,
+                   gradient_steps=1,
+                   gamma=0.99,
+                   ent_coef='auto',
+                   verbose=verbose,
+                   device=device,
+                   tensorboard_log=log_dir,
+                   policy_kwargs={'net_arch': {'pi': [32, 32], 'qf': [32, 32]}},
+                   **kwargs,
+                   )
+    else:
+        raise NameError("Not implemented policy name")
