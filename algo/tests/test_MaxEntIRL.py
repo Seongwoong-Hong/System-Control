99bc833979a2ba0a6a496a75326e33c4d4711542
sw.hong <sw.hong@kaist.ac.kr>
2021-05-12 17:38:27 +0900

    test: new tests for algorithm

--- algo/tests/test_MaxEntIRL.py
+++ algo/tests/test_MaxEntIRL.py
@@ -9,7 +9,7 @@ from imitation.data import rollout
 
 @pytest.fixture
 def expert():
-    expert_dir = os.path.join("..", "..", "IRL", "demos", "IDP", "expert.pkl")
+    expert_dir = os.path.join("..", "..", "IRL", "demos", "IP", "expert.pkl")
     with open(expert_dir, "rb") as f:
         expert_trajs = pickle.load(f)
     return rollout.flatten_trajectories(expert_trajs)
@@ -17,15 +17,39 @@ def expert():
 
 @pytest.fixture
 def env():
-    return make_env("IDP_custom-v2", use_vec_env=False)
+    return make_env("IP_custom-v2", use_vec_env=False)
 
 
 def test_make_obj(expert, env):
     learning = MaxEntIRL(env,
                          agent_learning_steps=1000,
                          expert_transitions=expert,
-                         rew_kwargs={'lr': 1e-3, 'arch': [2, 4, 4]},
+                         rew_kwargs={'lr': 1e-3, 'arch': [4, 8, 8]},
                          )
     # Test rollout from agent
-    rollouts = learning.rollout_from_agent()
+    rollouts = learning.rollout_from_agent(n_episodes=8)
     # Test calculation of loss
+    loss = learning.cal_loss(rollouts)
+    assert isinstance(loss.item(), float)
+
+
+def test_learn_agent(env):
+    learning = MaxEntIRL(env,
+                         agent_learning_steps=1000,
+                         expert_transitions=expert,
+                         rew_kwargs={'lr': 1e-3, 'arch': [4, 8, 8]},
+                         sac_kwargs={'verbose': 1}
+                         )
+    learning.agent.learn(total_timesteps=1e4)
+
+
+def test_process(env, expert):
+    learning = MaxEntIRL(env,
+                         agent_learning_steps=1000,
+                         expert_transitions=expert,
+                         rew_lr=1e-5,
+                         rew_arch=[3, 8, 8],
+                         device='cuda:0',
+                         sac_kwargs={'verbose': 1},
+                         )
+    learning.learn(total_iter=10, gradient_steps=1)
