b2d6e0331230aefbb74052f725dc01e693561750
sw.hong <sw.hong@kaist.ac.kr>
2021-05-20 17:39:13 +0900

    fix: modify callbacks and solve the subpath issue

--- IRL/scripts/MaxEntIRL.py
+++ IRL/scripts/MaxEntIRL.py
@@ -4,11 +4,9 @@ import shutil
 
 from imitation.data import rollout
 from imitation.util import logger
-from imitation.policies import serialize
-from stable_baselines3.common import callbacks
 
 from common.util import make_env, create_path
-from common.callbacks import SaveRewardCallback
+from common.callbacks import SaveCallback
 from algo.torch.MaxEntIRL import MaxEntIRL
 
 if __name__ == "__main__":
@@ -16,23 +14,21 @@ if __name__ == "__main__":
     algo_type = "MaxEntIRL"
     device = "cpu"
     name = "IDP_custom"
-    env = make_env(f"{name}-v2", use_vec_env=False, num_envs=8, n_steps=600, sub="sub01")
     proj_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    subpath = os.path.join(proj_path, "demos", env_type, "sub01", "sub01")
+    env = make_env(f"{name}-v1", use_vec_env=False, num_envs=8, n_steps=600, subpath=subpath)
 
     # Load data
-    expert_dir = os.path.join(proj_path, "demos", env_type, "ppoeasy.pkl")
+    expert_dir = os.path.join(proj_path, "demos", env_type, "expert.pkl")
     with open(expert_dir, "rb") as f:
         expert_trajs = pickle.load(f)
     transitions = rollout.flatten_trajectories(expert_trajs)
 
     # Setup log directories
     log_dir = os.path.join(proj_path, "tmp", "log", env_type, algo_type, name)
-    log_dir += "3"
-    if not os.path.isdir(log_dir):
-        create_path(log_dir)
-    else:
-        print("The log directory already exists")
-        raise SystemExit
+    log_dir += "_test"
+    assert not os.path.isdir(log_dir), "The log directory already exists"
+    create_path(log_dir)
     print(f"All Tensorboards and logging are being written inside {log_dir}/.")
     shutil.copy(os.path.abspath(__file__), log_dir)
 
@@ -41,9 +37,7 @@ if __name__ == "__main__":
         os.mkdir(model_dir)
 
     # Setup callbacks
-    save_policy_callback = serialize.SavePolicyCallback(model_dir, None)
-    save_policy_callback = callbacks.EveryNTimesteps(int(5e4), save_policy_callback)
-    save_reward_callback = SaveRewardCallback(cycle=5, dirpath=model_dir)
+    save_net_callback = SaveCallback(cycle=5, dirpath=model_dir)
 
     # Setup Logger
     logger.configure(log_dir, format_strs=["stdout", "tensorboard"])
@@ -59,12 +53,11 @@ if __name__ == "__main__":
                          )
 
     # Run Learning
-    losses = learning.learn(total_iter=10,
-                            gradient_steps=500,
+    losses = learning.learn(total_iter=50,
+                            gradient_steps=100,
                             n_episodes=8,
-                            max_sac_iter=20,
-                            rew_callback=save_reward_callback.save,
-                            agent_callback=save_policy_callback,
+                            max_sac_iter=10,
+                            callback=save_net_callback.net_save,
                             )
 
     # Save the result of learning
@@ -73,4 +66,3 @@ if __name__ == "__main__":
         pickle.dump(learning.reward_net, f)
     os.replace(reward_path + ".tmp", reward_path)
     learning.agent.save(model_dir + "/agent")
-
