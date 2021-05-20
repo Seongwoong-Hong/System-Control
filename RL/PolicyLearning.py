5cea95947df73176ac01a9c1c4bbbbbdb1afb43c
sw.hong <sw.hong@kaist.ac.kr>
2021-05-20 17:47:49 +0900

    feat: Add an environment wrapping function

--- RL/PolicyLearning.py
+++ RL/PolicyLearning.py
@@ -1,23 +1,39 @@
 import os
+import pickle
 
 from stable_baselines3.common import callbacks
+from stable_baselines3.common.vec_env import VecNormalize
 from imitation.policies import serialize
 
 from common.callbacks import VideoCallback
 from common.util import create_path, make_env
+from common.wrappers import RewardWrapper
 from RL.project_policies import def_policy
 
 from mujoco_py import GlfwContext
 
 
+def wrapped_env(environment, reward_wrap=None, norm_wrap=False):
+    renv = environment
+    if reward_wrap:
+        renv = RewardWrapper(renv, reward_wrap)
+    if norm_wrap:
+        renv = VecNormalize(renv, norm_obs=True, norm_reward=False)
+    return renv
+
+
 if __name__ == "__main__":
-    env_type = "IP"
+    env_type = "IDP"
     algo_type = "sac"
-    name = "IP_custom"
+    name = "IDP_custom"
     device = "cpu"
     env_id = f"{name}-v2"
     env = make_env(env_id, use_vec_env=False, num_envs=8, n_steps=600)
+    with open(f"../IRL/tmp/log/{env_type}/MaxEntIRL/{name}/2/model/reward_net.pkl", "rb") as f:
+        reward_net = pickle.load(f).double()
+    env = wrapped_env(env, reward_wrap=reward_net.eval(), norm_wrap=False)
     current_path = os.path.dirname(__file__)
+    name += "_learned_reward"
     log_dir = os.path.join(current_path, env_type, "tmp", "log", name, algo_type)
     create_path(dirname=log_dir)
     GlfwContext(offscreen=True)
@@ -32,6 +48,6 @@ if __name__ == "__main__":
     save_policy_callback = serialize.SavePolicyCallback(log_dir + f"/policies_{n}", None)
     save_policy_callback = callbacks.EveryNTimesteps(int(5e5), save_policy_callback)
     callback_list = callbacks.CallbackList([video_recorder, save_policy_callback])
-    algo.learn(total_timesteps=int(5e6), tb_log_name="extra", callback=callback_list)
+    algo.learn(total_timesteps=int(1e6), tb_log_name="extra", callback=callback_list)
     algo.save(log_dir+f"/policies_{n}/{algo_type}0")
     print(f"saved as policies_{n}/{algo_type}0")
