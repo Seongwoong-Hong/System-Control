59c836992a2cba385fe4019d4c8ec029e2546ccb
sw.hong <sw.hong@kaist.ac.kr>
2021-05-21 19:28:52 +0900

    refactor: Modify the error according to the 'subpath'

--- IRL/demos/CollectExperts.py
+++ IRL/demos/CollectExperts.py
@@ -5,20 +5,21 @@ from stable_baselines3.common.vec_env import DummyVecEnv
 
 from algo.torch.ppo import PPO
 from common.util import make_env
+from IRL.scripts.project_policies import def_policy
 
 if __name__ == "__main__":
     n_steps, n_episodes = 600, 8
-    env_type = "mujoco_envs"
-    name = "Hopper"
-    sub = "sub01"
-    env = make_env(env_name=f"{name}-v2", use_vec_env=False, sub=sub, n_steps=n_steps)
+    env_type = "IP"
+    name = "IP_custom"
+    subpath = "HPC/sub01/sub01"
+    env = make_env(env_name=f"{name}-v2", use_vec_env=False, subpath=subpath, n_steps=n_steps)
     sample_until = rollout.make_sample_until(n_timesteps=None, n_episodes=n_episodes)
     proj_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    # ExpertPolicy = def_policy(env_type, env, noise_lv=0.25)
-    ExpertPolicy = PPO.load(f"{proj_path}/../RL/{env_type}/tmp/log/{name}/ppo/policies_2/ppo0.zip")
+    ExpertPolicy = def_policy(env_type, env, noise_lv=0.25)
+    # ExpertPolicy = PPO.load(f"{proj_path}/../RL/{env_type}/tmp/log/{name}/ppo/policies_2/ppo0.zip")
     # ExpertPolicy = PPO.load(f"{proj_path}/tmp/log/IDP/ppo/lqrppo/000000500000/model.pkl")
     trajectories = rollout.generate_trajectories(
         ExpertPolicy, DummyVecEnv([lambda: env]), sample_until, deterministic_policy=False)
-    save_name = f"{env_type}/hopper.pkl"
+    save_name = f"{env_type}/lqr.pkl"
     types.save(save_name, trajectories)
     print(f"Expert Trajectories are saved in the {save_name}")
