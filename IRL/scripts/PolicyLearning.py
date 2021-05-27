6b1917a610b2411a370e7fffdd2d32cfa50b54ef
sw.hong <sw.hong@kaist.ac.kr>
2021-05-27 19:19:59 +0900

    feat: Add wrapper for learned reward function

--- IRL/scripts/PolicyLearning.py
+++ IRL/scripts/PolicyLearning.py
@@ -1,5 +1,5 @@
-import gym_envs
 import os
+import pickle
 
 from stable_baselines3.common import callbacks
 from imitation.policies import serialize
@@ -7,33 +7,48 @@ from imitation.policies import serialize
 from IRL.scripts.project_policies import def_policy
 from common.callbacks import VideoCallback
 from common.util import make_env, create_path
+from common.wrappers import RewardWrapper
 from mujoco_py import GlfwContext
+from scipy import io
 
 
 if __name__ == "__main__":
-    env_type = "IDP"
-    algo_type = "sac"
-    name = f"{env_type}/{algo_type}"
-    env_id = f"{env_type}_custom-v1"
+    env_type = "HPC"
+    algo_type = "MaxEntIRL"
+    name = f"no_sub01_1&2"
+    env_id = f"{env_type}_custom-v0"
     n_steps = 600
-    env = make_env(env_id, num_envs=8, n_steps=600, subpath="sub01")
-    device = "cpu"
+
+    pltqs = []
+    for i in range(10):
+        file = "../demos/HPC/sub01/sub01" + f"i{i + 1}.mat"
+        pltqs += [io.loadmat(file)['pltq']]
+
+    env = make_env(env_id, num_envs=8, n_steps=600, pltqs=pltqs)
+
+    def feature_fn(x):
+        return x
+
+    with open(f"../tmp/log/{env_type}/{algo_type}/{name}/model/044/reward_net.pkl", "rb") as f:
+        reward_net = pickle.load(f).double()
+    env = RewardWrapper(env, reward_net.eval())
+
+    device = "cuda:3"
     proj_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    log_dir = os.path.join(proj_path, "tmp", "log", name, "test")
-    model_dir = os.path.join(log_dir, "model")
-    create_path(model_dir)
+    log_dir = os.path.join(proj_path, "tmp", "log", env_type, algo_type, name, "add_rew_learning")
     GlfwContext(offscreen=True)
-    algo = def_policy(algo_type, env, device=device, log_dir=log_dir)
+    algo = def_policy("sac", env, device=device, log_dir=log_dir, verbose=1)
     n = 1
     while os.path.isdir(log_dir + f"/extra_{n}"):
         n += 1
     create_path(log_dir + f"/policies_{n}")
-    video_recorder = VideoCallback(gym_envs.make(env_id, n_steps=n_steps),
+    video_recorder = VideoCallback(make_env(env_id, use_vec_env=False, n_steps=n_steps, pltqs=pltqs),
                                    n_eval_episodes=5,
-                                   render_freq=62500)
+                                   render_freq=100000)
     save_policy_callback = serialize.SavePolicyCallback(log_dir + f"/policies_{n}", None)
-    save_policy_callback = callbacks.EveryNTimesteps(125000, save_policy_callback)
+    save_policy_callback = callbacks.EveryNTimesteps(100000, save_policy_callback)
     callback_list = callbacks.CallbackList([video_recorder, save_policy_callback])
-    algo.learn(total_timesteps=5e6, tb_log_name="extra", callback=callback_list)
-    algo.save(model_dir + f"/policies_{n}/{algo_type}0")
+    for i in range(10):
+        algo.learn(total_timesteps=125000, tb_log_name="extra", callback=callback_list)
+    algo.save(log_dir + f"/policies_{n}/{algo_type}0")
     print(f"saved as policies_{n}/{algo_type}0")
