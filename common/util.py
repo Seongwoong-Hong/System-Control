f9c0f9fd545da4ba9d0c0841bf868034fe54cdc9
sw.hong <sw.hong@kaist.ac.kr>
2021-07-22 13:19:07 +0900

    fix: Fix an error that env doesn't get any kwargs

--- common/util.py
+++ common/util.py
@@ -8,50 +8,50 @@ from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 
 
 def make_env(env_name, use_vec_env=False, num_envs=10, use_norm=False, **kwargs):
-    def is_vectorized(env):
-        if use_norm:
-            stats_path = kwargs.pop("stats_path", None)
-            env = DummyVecEnv([lambda: env for _ in range(num_envs)])
-            if not stats_path:
-                env = VecNormalize(env, norm_obs=True, norm_reward=True)
-            else:
-                env = VecNormalize.load(stats_path, env)
-        elif use_vec_env:
-            env = DummyVecEnv([lambda: env for _ in range(num_envs)])
-        return env
-
     if isinstance(env_name, gym.Env):
         env = env_name
-        env_type = "Env"
     else:
         env_type = env_name[:env_name.find("_custom")]
-        env = gym.make(env_name)
-    if env_type == "HPC":
-        subpath = kwargs.pop("subpath", None)
-        pltqs = kwargs.get("pltqs")
-        assert subpath is not None or pltqs is not None, "HPC environment needs pltqs!"
-        if not pltqs:
-            pltqs = []
-            i = 0
-            while True:
-                file = subpath + f"i{i+1}.mat"
-                if not p.isfile(file):
-                    break
-                pltqs += [io.loadmat(file)['pltq']]
-                i += 1
-            kwargs['pltqs'] = pltqs
-    else:
-        kwargs.pop('subpath', None)
-        kwargs.pop('pltqs', None)
-        kwargs.pop('bsp', None)
-    return is_vectorized(env)
+        if env_type == "HPC":
+            subpath = kwargs.pop("subpath", None)
+            pltqs = kwargs.get("pltqs")
+            assert subpath is not None or pltqs is not None, "HPC environment needs pltqs!"
+            if not pltqs:
+                pltqs = []
+                i = 0
+                while True:
+                    file = subpath + f"i{i+1}.mat"
+                    if not p.isfile(file):
+                        break
+                    pltqs += [io.loadmat(file)['pltq']]
+                    i += 1
+                kwargs['pltqs'] = pltqs
+        else:
+            kwargs.pop('subpath', None)
+            kwargs.pop('pltqs', None)
+            kwargs.pop('bsp', None)
+        env = gym.make(env_name, **kwargs)
+
+    if use_norm:
+        stats_path = kwargs.pop("stats_path", None)
+        env = DummyVecEnv([lambda: env for _ in range(num_envs)])
+        if not stats_path:
+            env = VecNormalize(env, norm_obs=True, norm_reward=True)
+        else:
+            env = VecNormalize.load(stats_path, env)
+    elif use_vec_env:
+        env = DummyVecEnv([lambda: env for _ in range(num_envs)])
+
+    return env
 
 
-def write_analyzed_result(ana_fn,
-                          ana_dir,
-                          iter_name=None,
-                          result_path: str = "/model/result.txt",
-                          verbose=0):
+def write_analyzed_result(
+        ana_fn,
+        ana_dir,
+        iter_name=None,
+        result_path: str = "/model/result.txt",
+        verbose=0
+):
     if iter_name is not None:
         filename = ana_dir + f"/{iter_name}" + result_path
         assert p.isdir(p.abspath(p.join(filename, p.pardir))), "Directory doesn't exist"
