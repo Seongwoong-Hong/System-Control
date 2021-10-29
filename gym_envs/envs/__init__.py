c198d8a46adaafbfa92ac4d99477a9986e7d08b2
sw.hong <sw.hong@kaist.ac.kr>
2021-10-29 16:27:46 +0900

    add: Add a new environment

--- gym_envs/envs/__init__.py
+++ gym_envs/envs/__init__.py
@@ -3,3 +3,4 @@ from gym_envs.envs.cartpolecont_test import CartPoleContTestEnv
 from gym_envs.envs.inverted_double_pendulum import InvertedDoublePendulum
 from gym_envs.envs.twod_navigate import TwoDWorld, TwoDWorldDet, TwoDWorldDetOrder
 from gym_envs.envs.twod_navigate_discrete import TwoDWorldDiscDet, TwoDWorldDiscDetOrder, TwoDWorldDisc
+from gym_envs.envs.twod_target import TwoDTarget
---
+++ gym_envs/envs/twod_target.py
@@ -0,0 +1,63 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from typing import List
+
+
+class TwoDTarget(gym.Env):
+    def __init__(self):
+        self.height = 1.0
+        self.width = 1.0
+        self.dt = 0.01
+        self.st = None
+        self.observation_space = gym.spaces.Box(low=np.array([-self.width, -self.height]),
+                                                high=np.array([self.width, self.height]))
+        self.action_space = gym.spaces.Box(low=-1.0, high=1.0, shape=(2,))
+
+    def step(self, action: np.ndarray):
+        assert self.st is not None, "Can't step the environment before calling reset function"
+        r = self.reward_fn(self.st)
+        info = {'obs': self.st.reshape(1, -1), 'acts': action.reshape(1, -1)}
+        st = self.st + self.dt * action + 0.01 * np.random.random(self.st.shape)
+        done = bool(
+            self.st[0] < -self.width
+            or self.st[0] > self.width
+            or self.st[1] < -self.height
+            or self.st[1] > self.height
+        )
+        if not done:
+            self.st = st
+        return self.st, r, None, info
+
+    def _get_obs(self):
+        return self.st
+
+    def reset(self):
+        self.st = self.observation_space.sample()
+        return self._get_obs()
+
+    def reward_fn(self, state) -> float:
+        x, y = state[0], state[1]
+        return - ((x - self.width/3) ** 2 + (y - self.height/3) ** 2)
+
+    def draw(self, trajs: List[np.ndarray] = None):
+        if trajs is None:
+            trajs = []
+        d1, d2 = np.meshgrid(np.linspace(-self.width, self.width, 100), np.linspace(-self.height, self.height, 100))
+        r = self.reward_fn([d1, d2])
+        fig = plt.figure(figsize=[4, 4], dpi=300.0)
+        ax = fig.add_subplot(1, 1, 1, projection='3d')
+        ax.plot_surface(d1, d2, r, rstride=1, cstride=1, cmap=cm.rainbow)
+        # ax.pcolor(d1, d2, r, cmap=cm.rainbow)
+        ax.set_xlabel("x", labelpad=15.0, fontsize=28)
+        ax.set_ylabel("y", labelpad=15.0, fontsize=28)
+        ax.set_title("Reward", fontsize=32)
+        ax.tick_params(axis='both', which='major', labelsize=20)
+        for traj in trajs:
+            ax.plot3D(traj[:, 0], traj[:, 1], traj[:, 2], color='k')
+        ax.view_init(elev=90, azim=0)
+        fig.show()
+
+    def render(self, mode='human'):
+        pass
