d2325a152e34f71018db0ee165df40e00bb1f132
sw.hong <sw.hong@kaist.ac.kr>
2021-11-12 17:46:11 +0900

    test: Add new tests for tabular rl algorithms

---
+++ tests/IRL/test_svm.py
@@ -0,0 +1,53 @@
+import os
+import pytest
+import torch as th
+import numpy as np
+import pickle
+from common.util import make_env
+from imitation.data.rollout import flatten_trajectories
+import matplotlib.pyplot as plt
+
+
+def test_svm():
+    with open("../../IRL/demos/1DTarget/viter_disc.pkl", "rb") as f:
+        expt_trajs = pickle.load(f)
+        expt_trans = flatten_trajectories(expt_trajs)
+    with open("../../IRL/demos/1DTarget/viter_disc0.pkl", "rb") as f:
+        agent_trajs = pickle.load(f)
+        agent_trans = flatten_trajectories(agent_trajs)
+    with open("../../IRL/tmp/log/1DTarget_disc/MaxEntIRL/ext_viter_disc_linear_svm_reset/model/000/reward_net.pkl", "rb") as f:
+        reward_fn = pickle.load(f)
+    with open("../../IRL/tmp/log/1DTarget_disc/MaxEntIRL/ext_viter_disc_linear_svm_reset/model/000/agent.pkl", "rb") as f:
+        agent = pickle.load(f)
+    agent_input = feature_fn(th.from_numpy(np.concatenate([agent_trans.obs], axis=1)))
+    expt_input = feature_fn(th.from_numpy(np.concatenate([expt_trans.obs], axis=1)))
+    agent_gammas = th.FloatTensor([0.8 ** (i % 20) for i in range(len(agent_trans))])
+    expt_gammas = th.FloatTensor([0.8 ** (i % 20) for i in range(len(expt_trans))])
+
+    agent_mu, expt_mu = th.zeros([len(agent_trajs), 2]), th.zeros([len(expt_trajs), 2])
+    for i in range(len(agent_trajs)):
+        agent_mu[i, 0] = (agent_gammas * agent_input[:, 0].flatten())[i * 20:(i + 1) * 20].sum()
+        agent_mu[i, 1] = (agent_gammas * agent_input[:, 1].flatten())[i * 20:(i + 1) * 20].sum()
+        # agent_mu[i, 2] = (agent_gammas * agent_input[:, 2].flatten())[i * 10:(i + 1) * 10].sum()
+        # agent_mu[i, 3] = (agent_gammas * agent_input[:, 3].flatten())[i * 10:(i + 1) * 10].sum()
+    for i in range(len(expt_trajs)):
+        expt_mu[i, 0] = (expt_gammas * expt_input[:, 0].flatten())[i * 20:(i + 1) * 20].sum()
+        expt_mu[i, 1] = (expt_gammas * expt_input[:, 1].flatten())[i * 20:(i + 1) * 20].sum()
+        # expt_mu[i, 2] = (expt_gammas * expt_input[:, 2].flatten())[i * 10:(i + 1) * 10].sum()
+        # expt_mu[i, 3] = (expt_gammas * expt_input[:, 3].flatten())[i * 10:(i + 1) * 10].sum()
+
+    w1, w2 = reward_fn.layers[0].weight.detach().flatten()
+    w1, w2 = w1.item(), w2.item()
+    b = reward_fn.layers[0].bias.detach().item()
+
+    a_r = w1 * agent_mu[:11, 0] + w2 * agent_mu[:11, 1] + b
+    e_r = w1 * expt_mu[:11, 0] + w2 * expt_mu[:11, 1] + b
+
+    print('end')
+
+
+if __name__ == "__main__":
+    def feature_fn(x):
+        return th.cat([x, x**2], dim=1)
+
+    test_svm()
--- tests/algos/test_tabular.py
+++ tests/algos/test_tabular.py
@@ -1,20 +1,42 @@
 import pytest
+import torch as th
+import pickle
 from common.util import make_env
+from common.wrappers import RewardWrapper
 from algos.tabular.qlearning import QLearning
 from algos.tabular.viter import Viter
 
 
 def test_qlearning():
     env = make_env("1DTarget_disc-v2")
-    algo = QLearning(env, 0.8, 0.2, 0.9, device='cpu')
-    algo.learn(10000)
+    algo = QLearning(env, gamma=0.8, epsilon=0.2, alpha=0.9, device='cpu')
+    algo.learn(1000)
 
     print('end')
 
 
 def test_viter():
     env = make_env("1DTarget_disc-v2")
-    algo = Viter(env, 0.8, 0.2)
-    algo.learn(10)
+    algo = Viter(env, gamma=0.8, epsilon=0.2, device='cpu')
+    algo.learn(200)
 
     print('env')
+
+
+def test_wrapped_reward():
+    with open("../../IRL/tmp/log/1DTarget_disc/MaxEntIRL/ext_ppo_disc_samp_linear_ppoagent_svm_reset/model/000/reward_net.pkl", "rb") as f:
+        reward_net = pickle.load(f).cpu()
+    env = make_env("1DTarget_disc-v2", wrapper=RewardWrapper, wrapper_kwrags={'rwfn': reward_net})
+    algo1 = QLearning(env, gamma=0.8, epsilon=0.2, alpha=0.6, device='cpu')
+    algo1.learn(1000)
+    algo2 = Viter(env, gamma=0.8, epsilon=0.2, device='cpu')
+    algo2.learn(50)
+
+    print('env')
+
+
+if __name__ == "__main__":
+    def feature_fn(x):
+        return th.cat([x, x**2], dim=1)
+
+    test_wrapped_reward()
