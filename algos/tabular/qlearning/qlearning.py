40e41e351112c2b27cebec163963c557097f79c9
sw.hong <sw.hong@kaist.ac.kr>
2021-11-12 17:41:00 +0900

    fix: Refactor class methods to apply to an IRL algorithm

--- algos/tabular/qlearning/qlearning.py
+++ algos/tabular/qlearning/qlearning.py
@@ -1,7 +1,9 @@
+import os
 import gym
 import pickle
 import numpy as np
 
+from typing import Optional, Tuple
 from copy import deepcopy
 
 from algos.tabular.qlearning.policy import TabularPolicy
@@ -15,15 +17,21 @@ class QLearning:
             epsilon,
             alpha,
             device,
+            **kwargs,
     ):
-        self.env = env
-        self.observation_space = env.observation_space
-        self.action_space = env.action_space
-        assert isinstance(self.observation_space, gym.spaces.MultiDiscrete) and isinstance(self.action_space, gym.spaces.MultiDiscrete)
         self.gamma = gamma
         self.epsilon = epsilon
         self.alpha = alpha
         self.device = device
+        self._setup_model(env)
+
+    def _setup_model(self, env):
+        self.num_timesteps = 0
+        self.env = env
+        self.observation_space = env.observation_space
+        self.action_space = env.action_space
+        assert isinstance(self.observation_space, gym.spaces.MultiDiscrete)\
+               and isinstance(self.action_space, gym.spaces.MultiDiscrete)
         self.policy = TabularPolicy(
             observation_space=self.observation_space,
             action_space=self.action_space,
@@ -40,18 +48,17 @@ class QLearning:
             done,
     ) -> None:
         if not done:
-            self.policy.q_table[ob.item(), action.item()] += \
-                self.alpha * (reward + self.gamma * np.max(self.policy.q_table[next_ob.item(), :])
-                              - self.policy.q_table[ob.item(), action.item()])
+            self.policy.q_table[ob, action] += \
+                self.alpha * (reward + self.gamma * np.max(self.policy.q_table[next_ob, :])
+                              - self.policy.q_table[ob, action])
         else:
-            self.policy.q_table[ob.item(), action.item()] += self.alpha * (reward - self.policy.q_table[ob.item(), action.item()])
+            self.policy.q_table[ob, action] += self.alpha * (reward - self.policy.q_table[ob, action])
 
-    def learn(
-            self,
-            train_length,
-    ) -> None:
-        prev_q_table = deepcopy(self.policy.q_table + 1)
-        for t in range(train_length):
+    def learn(self, total_timesteps, **kwargs) -> None:
+        t = 0
+        while True:
+            prev_q_table = deepcopy(self.policy.q_table)
+            self.num_timesteps = t
             ob = self.env.reset()
             done = False
             while not done:
@@ -61,11 +68,16 @@ class QLearning:
                 ob = next_ob
             for i in range(self.observation_space.nvec[0]):
                 self.policy.policy_table[i] = np.argmax(self.policy.q_table[i])
-            error = np.mean(np.abs(self.policy.q_table - prev_q_table))
-            print(f"Mean difference btw previous and current q is {error:.3f}")
-            if error < 1e-3 and t > 100:
+            error = np.max(np.abs(self.policy.q_table - prev_q_table))
+            if t % 10 == 0:
+                print(f"{t}th iter: Max difference btw previous and current q is {error:.3f}")
+            if error < 1e-8 and t > 100:
+                print(f"{t}th iter: Max difference btw previous and current q is {error:.3f}")
                 break
-            prev_q_table = deepcopy(self.policy.q_table)
+            t += 1
+
+    def reset(self, env):
+        self._setup_model(env)
 
     def get_env(self):
         return self.env
@@ -73,8 +85,17 @@ class QLearning:
     def set_env(self, env):
         self.env = env
 
-    def predict(self, observation, deterministic):
-        return self.policy.predict(observation, deterministic)
+    def get_vec_normalize_env(self):
+        return None
+
+    def predict(
+            self,
+            observation: np.ndarray,
+            state: Optional[np.ndarray] = None,
+            mask: Optional[np.ndarray] = None,
+            deterministic: bool = False
+    ) -> Tuple[np.ndarray, Optional[np.ndarray]]:
+        return self.policy.predict(observation, state, mask, deterministic)
 
     @classmethod
     def load(
@@ -85,9 +106,8 @@ class QLearning:
     ):
         pass
 
-    def save(
-        self,
-        path,
-    ):
-        pass
+    def save(self, log_dir):
+        with open(log_dir + ".tmp", "wb") as f:
+            pickle.dump(self, f)
+        os.replace(log_dir + ".tmp", log_dir + ".pkl")
 
