b3e5b55a4c9e4955613140ee71f97324f4a18d8e
sw.hong <sw.hong@kaist.ac.kr>
2021-11-12 17:41:23 +0900

    fix: Refactor class methods to apply to an IRL algorithm

--- algos/tabular/viter/policy.py
+++ algos/tabular/viter/policy.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+from typing import Optional, Tuple
 
 
 class TabularPolicy:
@@ -15,13 +16,19 @@ class TabularPolicy:
         self.v_table = np.zeros([self.observation_space.nvec[0]])
         self.policy_table = np.zeros([self.observation_space.nvec[0]], dtype=int)
 
-    def predict(self, observation, deterministic=False):
-        action = np.array([self.policy_table[observation.item()]], dtype=int)
+    def predict(
+            self,
+            observation: np.ndarray,
+            state: Optional[np.ndarray] = None,
+            mask: Optional[np.ndarray] = None,
+            deterministic: bool = False
+    ) -> Tuple[np.ndarray, Optional[np.ndarray]]:
+        action = self.policy_table[observation]
         if not deterministic:
             rd = random.random()
             if rd < self.epsilon:
-                action = self.action_space.sample()
+                action = (self.action_space.np_random.random_sample(observation.shape) * self.action_space.nvec).astype(self.action_space.dtype)
         return action, None
 
     def forward(self, observation, deterministic=False):
-        return self.predict(observation, deterministic)
+        return self.predict(observation, deterministic=deterministic)
--- algos/tabular/viter/viter.py
+++ algos/tabular/viter/viter.py
@@ -1,53 +1,107 @@
+import os
 import gym
+import pickle
 import numpy as np
+from typing import Optional, Tuple
 from copy import deepcopy
 from algos.tabular.viter.policy import TabularPolicy
+from stable_baselines3.common.vec_env import VecEnv
+from imitation.util import logger
+
 
 class Viter:
     def __init__(
             self,
             env,
             gamma,
-            epsilon
+            epsilon,
+            device,
+            **kwargs,
     ):
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.device = device
+        self._setup_model(env)
+
+    def _setup_model(self, env):
         self.env = env
         self.observation_space = env.observation_space
         self.action_space = env.action_space
+        self.num_timesteps = 0
         assert isinstance(self.observation_space, gym.spaces.MultiDiscrete) \
                and isinstance(self.action_space, gym.spaces.MultiDiscrete)
-        self.gamma = gamma
         self.policy = TabularPolicy(
             observation_space=self.observation_space,
             action_space=self.action_space,
-            epsilon=epsilon,
+            epsilon=self.epsilon,
         )
 
-    def train(self, reward_table):
+    def train(self):
+        self.env.reset()
         for i in range(self.observation_space.nvec[0]):
             candi = []
             for j in range(self.action_space.nvec[0]):
-                candi.append(self.policy.v_table[i] + self.gamma * reward_table[i][j])
+                if isinstance(self.env, VecEnv):
+                    self.env.env_method("set_state", np.array([i], dtype=int))
+                    ns, r, done, _ = self.env.step(np.array([[j]], dtype=int))
+                else:
+                    self.env.set_state(np.array([i], dtype=int))
+                    ns, r, done, _ = self.env.step(np.array([j], dtype=int))
+                if not done:
+                    candi.append(r + self.gamma * self.policy.v_table[ns])
+                else:
+                    candi.append(r)
+                    self.env.reset()
             self.policy.v_table[i] = np.max(candi)
 
-    def learn(self, train_iter):
-        self.env.reset()
-        reward_table = np.zeros([self.observation_space.nvec[0], self.action_space.nvec[0]])
-        for i in range(self.observation_space.nvec[0]):
-            for j in range(self.action_space.nvec[0]):
-                self.env.set_state(np.array([i], dtype=int))
-                ns, r, done, _ = self.env.step(np.array([j], dtype=int))
-                reward_table[i][j] = r
-        for i in range(train_iter):
+    def learn(self, total_timesteps, **kwargs):
+        t = 0
+        while True:
+            self.num_timesteps = t
             old_value = deepcopy(self.policy.v_table)
-            self.train(reward_table)
-            error = np.mean(np.abs(old_value - self.policy.v_table))
-            print(f"{i}th iter: Mean difference btw old value and current value is {error:.3f}")
-            if error < 0.001:
+            self.train()
+            error = np.max(np.abs(old_value - self.policy.v_table))
+            logger.record("Value Error", error)
+            logger.dump(t)
+            print(f"{self.num_timesteps}th iter: Mean difference btw old value and current value is {error:.3f}")
+            if error < 1e-8 and t > 10:
+                for i in range(self.observation_space.nvec[0]):
+                    candi = []
+                    for j in range(self.action_space.nvec[0]):
+                        if isinstance(self.env, VecEnv):
+                            self.env.env_method("set_state", np.array([i], dtype=int))
+                            ns, r, done, _ = self.env.step(np.array([[j]], dtype=int))
+                        else:
+                            self.env.set_state(np.array([i], dtype=int))
+                            ns, r, done, _ = self.env.step(np.array([j], dtype=int))
+                        if not done:
+                            candi.append(r + self.gamma * self.policy.v_table[ns])
+                        else:
+                            candi.append(r)
+                            self.env.reset()
+                    self.policy.policy_table[i] = np.argmax(candi)
                 break
-        for i in range(self.observation_space.nvec[0]):
-            candi = []
-            for j in range(self.action_space.nvec[0]):
-                self.env.set_state(np.array([i], dtype=int))
-                ns, _, done, _ = self.env.step(np.array([j], dtype=int))
-                candi.append(reward_table[i][j] + self.gamma * self.policy.v_table[ns])
-            self.policy.policy_table[i] = np.argmax(candi)
+            t += 1
+
+    def predict(
+            self,
+            observation: np.ndarray,
+            state: Optional[np.ndarray] = None,
+            mask: Optional[np.ndarray] = None,
+            deterministic: bool = False
+    ) -> Tuple[np.ndarray, Optional[np.ndarray]]:
+        return self.policy.predict(observation, state, mask, deterministic)
+
+    def reset(self, env):
+        self._setup_model(env)
+
+    def set_env(self, env):
+        self.env = env
+
+    def get_vec_normalize_env(self):
+        return None
+
+    def save(self, log_dir):
+        with open(log_dir + ".tmp", "wb") as f:
+            pickle.dump(self, f)
+        os.replace(log_dir + ".tmp", log_dir + ".pkl")
