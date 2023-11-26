1c2e4d1a66cdece12307f8f4d6d0093bed75f975
HSW_lab <sw.hong@kaist.ac.kr>
2023-11-26 20:58:40 +0900

    util: Add new algorithm - check human data

---
+++ demos/drawHumanData.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+from scipy import io
+from matplotlib import pyplot as plt
+
+
+if __name__ == "__main__":
+    env_type = "IDP"
+    subj = "sub04"
+    trials = [5, 10, 15, 20, 25]
+    isPseudo = True
+
+    if isPseudo:
+        env_type = "Pseudo" + env_type
+
+    fig = plt.figure(figsize=[6.4, 9.6])
+    ax1 = fig.add_subplot(2, 1, 1)
+    ax2 = fig.add_subplot(2, 1, 2)
+    for trial in trials:
+        humanData = io.loadmat(str(Path(env_type) / subj / f"{subj}i{trial}.mat"))
+        state = humanData['state']
+        tq = humanData['tq']
+        ax1.plot(state[:, 3])
+        ax2.plot(tq[:, 0])
+    plt.show()
